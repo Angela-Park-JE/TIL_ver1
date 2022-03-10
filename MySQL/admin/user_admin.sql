e839ab081af8f454786a6005c4cd700b1a13892e
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-03-10 21:40:01 +0900

    Create user_admin.sql

---
+++ MySQL/admin/user_admin.sql
@@ -0,0 +1,21 @@
+-- show databases in now
+show databases; 
+
+-- Create database
+CREATE database glory;
+-- We have to set the encoding but else Korean will be broken
+CREATE schema glory default character set utf8; 
+commit;
+
+-- delete
+-- DROP database DBname;
+
+-- Giving the administrating roll : GRANT / REVOKE
+GRANT ALL PRIVILEGES ON glory.* TO angela;
+flush privileges;
+-- GRANT ALL PRIVILEGES ON DBname.* TO userid@localhost IDENTIFIED BY 'password';
+
+-- Check the granted privileges
+SHOW GRANTS FOR angela;
+
+-- Delete the privileges : REVOKE ALL ON DBname.* FROM USERid;
