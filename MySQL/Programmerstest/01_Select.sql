091f9d0185d14f952f7a636380882afbbdbe5585
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-03-14 23:54:03 +0900

    Update 01_Select.sql

--- MySQL/Programmerstest/01_Select.sql
+++ MySQL/Programmerstest/01_Select.sql
@@ -25,3 +25,18 @@ WHERE intake_condition != 'Aged'
 ORDER BY 1;
 
 
+
+-- 모든 동물 아이디와 이름, 아이디 순으로 정렬
+SELECT animal_id, name
+FROM ANIMAL_INS
+ORDER BY 1;
+
+
+
+-- 여러 기준으로 정렬하기
+SELECT animal_id, name, datetime
+FROM ANIMAL_INS
+ORDER BY name, datetime desc;
+
+
+
