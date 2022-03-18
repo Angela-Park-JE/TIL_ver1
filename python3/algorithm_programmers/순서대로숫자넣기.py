67da9381e7d9321bfec1c48c52a61e4b6173a988
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-03-18 21:16:44 +0900

    Create 순서대로숫자넣기.py

---
+++ python3/algorithm_programmers/순서대로숫자넣기.py
@@ -0,0 +1,16 @@
+# 정수 n이 주어질때, 1부터 n까지의 정수를 순서대로 담은 배열을 리턴하도록 solution 함수를 작성했습니다. 이때, 코드가 올바르게 동작할 수 있도록 빈칸을 알맞게 채워주세요.
+# @@@ 빈칸채우기
+
+def solution(n):
+    answer = []
+    for i in range(n):
+        @@@     
+    return answer
+    
+    
+# 내 답
+def solution(n):
+    answer = []
+    for i in range(n):
+        answer.append(i+1)     
+    return answer
