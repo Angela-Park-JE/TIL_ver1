cae9504b630bb67e3f86942ea181aa54e145965d
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-08-13 18:58:41 +0900

    Update 05-StartingWithVowels.sql

--- MySQL/HackerRank/05-StartingWithVowels.sql
+++ MySQL/HackerRank/05-StartingWithVowels.sql
@@ -23,3 +23,18 @@ SELECT DISTINCT(CITY) FROM STATION WHERE CITY RLIKE '^[aeiou]';
 SELECT CITY FROM STATION WHERE LOWER(SUBSTR(CITY,1,1)) in ('a','e','i','o','u');
 -- by melodyhe : 이 방법은 되지 않는다 
 SELECT DISTINCT city from station where city LIKE '[a, e, i, o, u]%' ORDER BY city;
+
+"""
+Prepare > SQL > Basic Select > Weather Observation Station 7
+https://www.hackerrank.com/challenges/weather-observation-station-7/
+Query the list of CITY names ending with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+"""
+
+--제출
+SELECT DISTINCT city
+FROM STATION
+WHERE city RLIKE '[aeiou]$'
+
+"""
+정규표현식으로 검색할 수 있는 방법이 RLIKE인데, '^a' 는 a로 시작, 'a$' 는 a로 끝나는 것을 검색한다.
+"""
