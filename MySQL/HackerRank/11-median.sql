a983c56ffca2b738db9ef99c9344c998df040081
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-08-18 19:51:34 +0900

    Update 11-median.sql

--- MySQL/HackerRank/11-median.sql
+++ MySQL/HackerRank/11-median.sql
@@ -5,7 +5,8 @@ A median is defined as a number separating the higher half of a data set from th
 Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
 """
 
---MySQL (개수에 따른 순번을 하나 혹은 둘을 구하고, 구하고자 하는 데이터에 순번을 매겨주고, 두 테이블을 함께 조회하여 답을 도출하도록)
+--MySQL (개수에 따른 순번을 하나 혹은 둘을 구하고, 구하고자 하는 데이터에 순번을 매겨주고, 두 테이블을 함께 조회하여 답을 도출하도록.)
+--CASE WHEN에서 오래걸렸다. 하나하나 쿼리결과 넣기보다 딱 구해놓기로 하도록 WITH를 사용. 그리고 no를 매기는 것에서 오래걸렸다.
 WITH 
     numbers AS (
                 SELECT CASE WHEN COUNT(lat_n) % 2 = 0
@@ -28,10 +29,11 @@ WITH
 SELECT 
     CASE WHEN n.b is null -- number of lat_n is not even numbers
          THEN ROUND(r.lat_n, 4)
+         WHEN n.b is not null -- number of lat_n is even numbers
+         THEN ROUND(r.lat_n + (SELECT r.lat_n FROM numbers n, rownum_lat r WHERE n.b = r.no), 4)
          END as result
 FROM numbers n, rownum_lat r
 WHERE n.a = r.no;
-
 
 
 
@@ -53,10 +55,3 @@ FROM dual;
 --             WHEN COUNT(lat_n)%2 = 1
 --             THEN a = COUNT(lat_n)/2 +0.5
 --        ELSE a = COUNT(lat_n) END);
-
--- SELECT CASE WHEN COUNT(lat_n)% 2 = 0
---             THEN COUNT(lat_n)/2 + 1 --AND b = COUNT(lan_t)/2
---             WHEN COUNT(lat_n) % 2 = 1
---             THEN COUNT(lat_n)/2 +0.5
---        ELSE COUNT(lat_n) END as a
--- FROM STATION
