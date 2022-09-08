f5f27e5a07e0edef80a7ac5251d2aa9efb6c6c8f
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-08 21:26:48 +0900

    Update un-19-AdvancedSelect.sql

--- MySQL/HackerRank/un-19-AdvancedSelect.sql
+++ MySQL/HackerRank/un-19-AdvancedSelect.sql
@@ -29,3 +29,13 @@ SELECT
          END AS Actor
 FROM OCCUPATIONS o1
 ORDER BY 1,2,3,4;
+
+/*- 시도 MySQL : 컬럼별로 조건을 WHERE에 넣었더니, 각 사람들의 이름이 합집합으로 모든 조합의 로우가 출력...된다. -*/
+SELECT od.name, op.name, os.name, oa.name 
+FROM OCCUPATIONS od, OCCUPATIONS op, OCCUPATIONS os, OCCUPATIONS oa
+WHERE 1=1
+    AND od.occupation = 'Doctor'
+    AND op.occupation = 'Professor'
+    AND os.occupation = 'Singer'
+    AND oa.occupation = 'Actor'
+
