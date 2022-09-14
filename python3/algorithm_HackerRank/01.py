eccfa003134092de30415a671daff495fbd8d182
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-14 21:39:26 +0900

    Update 01.py
    if 절 부터는 만지지않고 그대로를 사용한다. 함수 정의만 내가.

--- python3/algorithm_HackerRank/01.py
+++ python3/algorithm_HackerRank/01.py
@@ -1,1 +1,32 @@
- 아 열심히 쓴거 어디가쎴ㅆ어요
+"""
+Prepare> Interview Preparation Kits> 1 Week Preparation Kit> Day 1> Plus Minus
+https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. 
+Print the decimal value of each fraction on a new line with  places after the decimal.
+"""
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def plusMinus(arr):
+    n = len(arr)
+    p, m, z = [], [], []
+    for i in arr:
+        if i > 0:
+            p.append(i)
+        elif i < 0:
+            m.append(i)
+        elif i == 0:
+            z.append(i)
+    print(len(p)/n, '\n', len(m)/n, '\n', len(z)/n)
+
+if __name__ == '__main__':
+    numbers = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    plusMinus(arr)
