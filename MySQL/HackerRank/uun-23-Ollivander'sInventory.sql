22862edfc8059ef515264610786bbb8c6c72e3b0
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-14 20:11:04 +0900

    Update uun-23-Ollivander'sInventory.sql

--- MySQL/HackerRank/uun-23-Ollivander'sInventory.sql
+++ MySQL/HackerRank/uun-23-Ollivander'sInventory.sql
@@ -10,6 +10,8 @@ If more than one wand has same power, sort the result in order of descending age
 -- WANDS : id, code, coins_needed, power
 -- WANDS_PROPERTY : code, age, is_evil
 
+/*- MySQL: 파워와 에이지가 같다면 적은 코인을 내는 것을 찾아 넣어야했던 것이다. 헤르미온느... 돈도 돈이지만 시야를 좀 넓히는 건 어때 -*/
+
 
 """오답노트"""
 
@@ -19,3 +21,16 @@ FROM WANDS w, WANDS_PROPERTY p
 WHERE w.code = p.code
     AND p.is_evil = 0
 ORDER BY 4 DESC, 2 DESC;
+
+/*- 답 by.dun_zhang2012 -*/
+select w.id, p.age, w.coins_needed, w.power 
+from Wands as w 
+  join Wands_Property as p on (w.code = p.code) 
+where p.is_evil = 0 
+  and w.coins_needed = 
+    (
+    select min(coins_needed) 
+    from Wands as w1 join Wands_Property as p1 on (w1.code = p1.code) 
+    where w1.power = w.power and p1.age = p.age
+    ) 
+order by w.power desc, p.age desc
