6b7b63e038861015a59e937555a2a2317dc5a91d
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-15 21:35:06 +0900

    Update and rename 24-CHALLENGES.sql to un-24-CHALLENGES.sql

--- MySQL/HackerRank/un-24-CHALLENGES.sql
+++ MySQL/HackerRank/un-24-CHALLENGES.sql
@@ -11,6 +11,8 @@ If more than one student created the same number of challenges and the count is 
 
 
 """오답노트"""
+/*- DISTINCT앞에 그에 맞는 다른 로우를 가져오려고 하면 오류가 난다. -*/
+
 /*- MySQL: 카운트를 구한 후 맥스 카운트인 경우만 전부 출력하도록 CASE WHEN을 쓰려고 했으나 맥스카운트가 아닌 경우는 어떻게 한 명만 출력하지...?-*/
 -- 카운트까지 구함
 SELECT h1.hacker_id, COUNT(c1.challenge_id) counts
@@ -27,3 +29,26 @@ JOIN
     FROM HACKERS h1 JOIN CHALLENGES c1 ON h1.hacker_id = c1.hacker_id
     GROUP BY h1.hacker_id
     ) tb1 ON h2.hacker_id = tb1.hacker_id;
+
+
+
+/*- MySQL by.simple_guitar99 : 내가 하려던 방법과 가장 같아서 읽고 공부중-*/
+/* count total submissions of challenges of each user */
+WITH data
+AS
+(
+SELECT c.hacker_id as id, h.name as name, count(c.hacker_id) as counter
+FROM Hackers h
+JOIN Challenges c on c.hacker_id = h.hacker_id
+GROUP BY c.hacker_id, h.name
+)
+/* select records from above */
+SELECT id,name,counter
+FROM data
+WHERE
+counter=(SELECT max(counter) FROM data) /*select user that has max count submission*/
+OR
+counter in (SELECT counter FROM data
+GROUP BY counter
+HAVING count(counter)=1 ) /*filter out the submission count which is unique*/
+ORDER BY counter desc, id
