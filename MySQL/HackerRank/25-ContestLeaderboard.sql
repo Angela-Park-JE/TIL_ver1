deb7db35d0f38ae2e121c8c9cdd06d55841b34c0
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-17 20:01:14 +0900

    Update 25-ContestLeaderboard.sql

--- MySQL/HackerRank/25-ContestLeaderboard.sql
+++ MySQL/HackerRank/25-ContestLeaderboard.sql
@@ -2,7 +2,6 @@
 Prepare> SQL> Basic Join> Contest Leaderboard
 https://www.hackerrank.com/challenges/contest-leaderboard/problem
 You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
-
 The total score of a hacker is the sum of their maximum scores for all of the challenges. 
 Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score. 
 If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. 
@@ -26,7 +25,9 @@ WHERE TB2.total_score != 0
 ORDER BY 3 DESC, 1 ASC; 
 
 
-/*- MySQL by.m_alkhudairy : 조금 더 짧은 풀이가 있다. 처음부터 HACKERS 테이블을 조인하고 시작하면서 내가 쓴 가장 바깥 쿼리를 생략한 형식. -*/
+"""좋은 풀이"""
+
+/*- MySQL by.m_alkhudairy : 조금 더 짧은 풀이가 있다. 나와 같지만처음부터 HACKERS 테이블을 조인하고 시작하면서 내가 쓴 가장 바깥 쿼리를 생략한 형식. -*/
 SELECT hacker_id, name, sum(max) as score
 FROM (SELECT h.hacker_id as hacker_id, h.name as name, max(s.score) as max
       FROM hackers h JOIN submissions s ON h.hacker_id = s.hacker_id
@@ -34,3 +35,16 @@ FROM (SELECT h.hacker_id as hacker_id, h.name as name, max(s.score) as max
 GROUP BY hacker_id, name
 HAVING sum(max) > 0
 ORDER BY sum(max) desc, hacker_id asc;
+
+/*- MySQL by.Sardor_Bayramov : 해커랭크 친구이신데 고수시다. 짠 쿼리들을 보면 상당히 깔끔하다. -*/
+select id,
+       (select name
+          from hackers
+         where hacker_id = id),
+       sum(max_score) total_score
+  from (select hacker_id id, max(score) max_score
+          from submissions
+         where score > 0
+         group by hacker_id, challenge_id)
+ group by id
+ order by total_score desc, id;
