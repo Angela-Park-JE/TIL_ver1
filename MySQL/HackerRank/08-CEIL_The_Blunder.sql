31e996dd1104c6c9d0716e744efaf5a59d84e074
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-21 20:50:42 +0900

    Update 08-CEIL_The_Blunder.sql

--- MySQL/HackerRank/08-CEIL_The_Blunder.sql
+++ MySQL/HackerRank/08-CEIL_The_Blunder.sql
@@ -1,7 +1,7 @@
 """
 https://www.hackerrank.com/challenges/the-blunder/problem?isFullScreen=true
 Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, 
-but did not realize her keyboard's  key was broken until after completing the calculation. 
+but did not realize her keyboard's 0 key was broken until after completing the calculation. 
 She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
 Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
 """
