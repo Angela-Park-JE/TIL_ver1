5513652846e5adedb5985bc544e946d8edd0978b
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-22 21:40:25 +0900

    Update un-19-AdvancedSelect-Occupations.sql

--- MySQL/HackerRank/un-19-AdvancedSelect-Occupations.sql
+++ MySQL/HackerRank/un-19-AdvancedSelect-Occupations.sql
@@ -53,7 +53,9 @@ SELECT
 FROM OCCUPATIONS
 ORDER BY name ASC;
 
-/*- 시도4 MySQL : 내가 아는 지식 선에서 해결하려다 안되서, 각 테이블로 고유 번호를 부여하고 붙이는 방법을 생각했는데, pivoting관련해서 찾아보다 나와 똑같은 풀이를 생각한 사람이 있었다. -*/
+/*- 시도4 MySQL : 내가 아는 지식 선에서 해결하려다 안되서, 각 테이블로 고유 번호를 부여하고 붙이는 방법을 생각했는데, 
+    pivoting관련해서 찾아보다 나와 똑같은 풀이를 생각한 사람이 있었다. 실제로 name, rownum, occupation 순으로 나온다.
+    그 이후 조리방법을 못해서 더 진행을 하지 못했다.-*/
 -- reference : https://walkingfox.tistory.com/103
 select  name
         , case when @grp = occupation then @rownum:=@rownum + 1 
@@ -64,12 +66,52 @@ order by dum, name asc
 
 
 """참고"""
-/*- MySQL by.nobuh : 굳이 각 순서를 매긴 다음 순서 변수로 그룹화하여 불러오는 건 하고싶지 않아서 찾아보던 중 rank를 사용하는 좋은답.
+
+/*- Oracle by.GosiaB : 상당히 깔끔한 답. -*/
+SELECT doctor, professor, singer, actor 
+FROM 
+    (SELECT * 
+    FROM 
+        (SELECT Name, 
+                occupation, 
+                (ROW_NUMBER() OVER (PARTITION BY occupation ORDER BY name)) AS row_num 
+         FROM occupations ORDER BY name asc) 
+    pivot (min(name) for occupation IN ('Doctor' AS doctor,'Professor' AS professor,'Singer' AS singer,'Actor' AS actor)
+    ) 
+ ORDER BY row_num); 
+
+
+/*- MySQL by.lionheart2007_eg : rownum 함수를 사용하는 답. Oracle에서는 돌아가지 않는다. -*/
+SELECT 
+    MIN(CASE WHEN OCCUPATION = 'DOCTOR'      THEN NAME ELSE NULL END) ,
+    MIN(CASE WHEN OCCUPATION = 'PROFESSOR'   THEN NAME ELSE NULL END) ,
+    MIN(CASE WHEN OCCUPATION = 'SINGER'      THEN NAME ELSE NULL END) ,
+    MIN(CASE WHEN OCCUPATION = 'ACTOR'       THEN NAME ELSE NULL END)
+FROM
+    (SELECT OCCUPATION, NAME, ROW_NUMBER() OVER (PARTITION BY OCCUPATION ORDER BY NAME) AS OCCUS
+    FROM OCCUPATIONS) AS T
+GROUP BY OCCUS;
+
+
+/*- Oracle by.rashnil2 : rank 함수를 사용하는 답. MySQL에서는 돌아가지 않는다. -*/
+SELECT min(Doctor), min(Professor), min(Singer), min(Actor)
+FROM
+(SELECT  RANK() OVER(PARTITION BY occupation ORDER BY name) rank,
+    case OCCUPATION when 'Doctor' then NAME end AS Doctor,
+    case OCCUPATION when 'Professor' then NAME end AS Professor,
+    case OCCUPATION when 'Singer' then NAME end AS Singer,
+    case OCCUPATION when 'Actor' then NAME end AS Actor
+FROM occupations)
+GROUP BY rank
+ORDER BY rank;
+
+
+/*- MySQL by.nobuh : 돌아가지 않는 답이다. 굳이 각 순서를 매긴 다음 순서 변수로 그룹화하여 불러오는 건 하고싶지 않아서 찾아보던 중 rank를 사용하는 답.
     This query creates the ranking value, smaller values are in alphabetical order.
     Doctor and other occupations columns contain only a single value for each ranks, 
     so you can reduce null value by MAX or MIN aggregate function with group by rank value.-*/
--- MySQL
 SELECT 
+    rank,
     MIN(CASE WHEN Occupation = 'Doctor' THEN Name ELSE NULL END) AS Doctor,
     MIN(CASE WHEN Occupation = 'Professor' THEN Name ELSE NULL END) AS Professor,
     MIN(CASE WHEN Occupation = 'Singer' THEN Name ELSE NULL END) AS Singer,
