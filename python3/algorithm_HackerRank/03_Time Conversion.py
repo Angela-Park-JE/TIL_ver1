9f7c09bf37d6aaf9d07ee3ed284161802440d79f
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-09-24 21:33:04 +0900

    Update 03_Time Conversion.py

--- python3/algorithm_HackerRank/03_Time Conversion.py
+++ python3/algorithm_HackerRank/03_Time Conversion.py
@@ -1,4 +1,6 @@
 """
+Prepare> Interview Preparation Kits> 1 Week Preparation Kit> Day 1> Time Conversion
+https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem
 Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
 - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
