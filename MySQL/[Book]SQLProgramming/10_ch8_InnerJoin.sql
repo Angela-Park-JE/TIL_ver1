14fe8ce71d0b7ad07b75f27a5afd1b73bf21f81f
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-10-29 23:43:01 +0900

    Update 10_ch8_InnerJoin.sql

--- MySQL/[Book]SQLProgramming/10_ch8_InnerJoin.sql
+++ MySQL/[Book]SQLProgramming/10_ch8_InnerJoin.sql
@@ -1,4 +1,4 @@
- /*- JOIN -*/
+ /**- JOIN -**/
 -- MUST know the 'join' columns name
 -- CAN 'join' more than two tables
 -- MUST state the table alias'
@@ -17,30 +17,32 @@ WHERE ... ; */
 -- case1
 USE world;
 SELECT a.id, a.name, b.code, b.name country_name, a.district, a.population
-FROM city a INNER JOIN country b ON a.countrycode = b.code
+FROM CITY a INNER JOIN COUNTRY b ON a.countrycode = b.code
 ORDER BY 1;
 
 -- case2 : can use WHERE.
 SELECT b.name, a.language, a.isofficial, a.percentage
-FROM countrylanguage a JOIN country b ON a.countrycode = b.code 
+FROM COUNTRYLANGUAGE a JOIN COUNTRY b ON a.countrycode = b.code 
 ORDER BY 1;
+
 SELECT b.name, a.language, a.isofficial, a.percentage
-FROM countrylanguage a JOIN country b ON a.countrycode = b.code 
+FROM COUNTRYLANGUAGE a JOIN COUNTRY b ON a.countrycode = b.code 
 WHERE a.countrycode = 'KOR'
 ORDER BY 1;
 
 -- case3: the sequence isn't mattered in INNER JOIN.
 SELECT a.name, b.language, b.isofficial, b.percentage
-FROM country a JOIN countrylanguage b ON a.code = b.countrycode
+FROM COUNTRY a JOIN COUNTRYLANGUAGE b ON a.code = b.countrycode
 WHERE b.countrycode = 'KOR'
 ORDER BY 1;
 
 -- case4: JOIN multiple tables
 SELECT a.code, a.name, a.continent, a.region, a.population, b.language, c.name, c.district, c.population
-FROM country a INNER JOIN countrylanguage b ON a.code = b.countrycode
-			   INNER JOIN city c ON a.code = c.countrycode
+FROM COUNTRY a INNER JOIN COUNTRYLANGUAGE b ON a.code = b.countrycode
+			   INNER JOIN CITY c ON a.code = c.countrycode
 WHERE c.countrycode = 'KOR'
 ORDER BY 1;
+
 
 
 /*- FROM WHERE join -*/
@@ -53,22 +55,23 @@ WHERE alias1.column1 = alias2.column2
 
 -- case1
 SELECT b.name coutry_name, a.language, a.isofficial, a.percentage
-FROM countrylanguage a, country b
+FROM COUNTRYLANGUAGE a, COUNTRY b
 WHERE a.countrycode = b.code
   AND a.countrycode = 'KOR'
 ORDER BY 1;
 
 -- case2: same with previous case4
 SELECT a.code, a.name, a.continent, a.region, a.population, b.language, c.name, c.district, c.population
-FROM country a, countrylanguage b, city c
+FROM COUNTRY a, COUNTRYLANGUAGE b, CITY c
 WHERE a.code = b.countrycode 
   AND b.countrycode = c.countrycode
   AND a.code = 'KOR'
 ORDER BY 1;
 
 
+
 -- p.269 quiz
 SELECT a.name country_name, COUNT(b.name)
-FROM country a INNER JOIN city b ON a.code = b.countrycode
+FROM COUNTRY a INNER JOIN CITY b ON a.code = b.countrycode
 GROUP BY country_name WITH ROLLUP;
 -- don't need to `b.name`, because city table has a city in one row.
