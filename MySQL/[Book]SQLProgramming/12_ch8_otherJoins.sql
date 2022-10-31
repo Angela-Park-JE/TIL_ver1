95c2d25930b75b0615f3c7a467c00d807848d3e0
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-10-31 18:27:27 +0900

    Update 12_ch8_otherJoins.sql

--- MySQL/[Book]SQLProgramming/12_ch8_otherJoins.sql
+++ MySQL/[Book]SQLProgramming/12_ch8_otherJoins.sql
@@ -1,4 +1,5 @@
-/*- JOIN -*/
+ /**- JOIN -**/
+ 
 -- MUST know the 'join' columns name
 -- CAN 'join' more than two tables
 -- MUST state the table alias'
@@ -9,8 +10,8 @@
 
 
 /*- NATURAL JOIN -*/
--- doesn't write join condition
--- need to join columns have to be same name and same data type column in each tables,
+-- no join condition
+-- need to join columns have to be 'same name' and 'same data type' column in each tables,
 -- and if it satisfied, MySQL automatically make a condition with two tables' column.
 -- NATURAL | NATURAL INNER : natural inner join
 -- NATURAL LEFT | NATURAL RIGHT : nataural outer join
@@ -22,23 +23,23 @@ WHERE ... ; */
 -- case1: natural join need to have same name and data type
 USE world;
 SELECT a.continent, COUNT(*), COUNT(b.name)
-FROM country a NATURAL JOIN city b 
+FROM COUNTRY a NATURAL JOIN CITY b 
 GROUP BY 1;
 -- there's no error but it has no results. 
--- "becuase countrycode and code columns have different name."
+-- "becuase countrycode and code columns have 'different name'."
 
 -- case2: 
 SELECT *
-FROM city a NATURAL JOIN countrylanguage b;
+FROM CITY a NATURAL JOIN COUNTRYLANGUAGE b;
 -- they have normal join columns.
 -- "if use * in SELECT, the join column comes out once."
 
 
 
 /*- CARTESIAN PRODUCT(CROSS JOIN) -*/
--- doesn't write join condition
+-- no join condition
 -- it makes automatically joins two tables
--- but the direction of joining is to make all possible combinations with two tables.
+-- but the direction of joining is to make 'all possible combinations' with two tables.
 /* structure : INNER JOIN without `ON`
 SELECT ...
 FROM table1 [AS] alias1 {INNER JOIN|CROSS JOIN} table2 [AS] alias2
@@ -46,10 +47,10 @@ WHERE ... ; */
 
 -- case1: cartesian product makes all combinations with tables' rows so the result data is massively big.
 SELECT a.continent, COUNT(*) all_, COUNT(b.name) cities
-FROM country a INNER JOIN city b
+FROM COUNTRY a INNER JOIN CITY b
 GROUP BY a.continent;
 
 -- case2(p.283 quiz): can use `CROSS JOIN` (SQL Standard)
 SELECT a.continent, COUNT(*) all_, COUNT(b.name) cities
-FROM country a CROSS JOIN city b
+FROM COUNTRY a CROSS JOIN CITY b
 GROUP BY a.continent;
