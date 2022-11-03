313b385b6eb8a40df5e38687f3104101f77eb4f6
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-11-03 21:37:41 +0900

    Update 20_ch11_VIEW.sql

--- MySQL/[Book]SQLProgramming/20_ch11_VIEW.sql
+++ MySQL/[Book]SQLProgramming/20_ch11_VIEW.sql
@@ -1,4 +1,4 @@
-/*** VIEW ***/
+/**- VIEW -**/
 -- a temporary table from a saved query
 -- not saving table, save a query. so look a view, it bring the result of the query.
 -- USE in `FROM` sentence like a table.
@@ -24,7 +24,7 @@ DROP VIEW view_name;
 
 
 -- code 11-28 : creating a view 1
--- code 11-27: SELECT * FROM DEPT_EMP WHERE SYSDATE() BETWEEN from_date AND to_date;
+-- code 11-27 : SELECT * FROM DEPT_EMP WHERE SYSDATE() BETWEEN from_date AND to_date;
 CREATE OR REPLACE VIEW dept_emp_v AS
 SELECT emp_no, dept_no FROM DEPT_EMP WHERE SYSDATE() BETWEEN from_date AND to_date;
 
@@ -75,7 +75,6 @@ FROM DEPT_EMP_V;
 DROP VIEW DETP_SAL_V;   -- I made a mistake o0o......
 
 
-
 -- p.441 quiz : make a DEPT_EMP_INFO_V with current part's emp_no, name using DEPARTMENTS, DEPT_EMP, EMPLOYEES table
 CREATE OR REPLACE VIEW DEPT_EMP_INFO_V AS
 	SELECT d.dept_name, e.emp_no, CONCAT(emp.first_name, ' ', emp.last_name) full_name
@@ -89,12 +88,14 @@ CREATE OR REPLACE VIEW DEPT_EMP_INFO_V AS
 
 /* self check */
 -- 1. Refer to DEPT_EMP_INFO_V, make a new view DEPT_EMP_SAL_V with current dept_name, emp_no, full_name, salary.
+
 -- mine : not wrong
 CREATE OR REPLACE VIEW DEPT_EMP_SAL_V AS
 	SELECT v.*, s.salary
     FROM DEPT_EMP_INFO_V v, SALARIES s
     WHERE v.emp_no = s.emp_no
       AND SYSDATE() BETWEEN s.from_date AND s.to_date;
+	  
 -- answer : making a view not using the view.
 CREATE OR REPLACE VIEW DEPT_EMP_SAL_V AS
 	SELECT d.dept_name, e.emp_no, CONCAT(emp.first_name, ' ', emp.last_name) full_name, s.salary
@@ -106,7 +107,9 @@ CREATE OR REPLACE VIEW DEPT_EMP_SAL_V AS
       AND SYSDATE() BETWEEN s.from_date AND s.to_date
     ORDER BY 1, 4 DESC;
 
+
 -- 2. Refer to DEPT_EMP_SAL_V, make a query with top 3 salary employees by department.
+
 -- mine
 WITH CTE AS 
 	(SELECT dept_name, emp_no, full_name, salary, 
@@ -117,6 +120,7 @@ SELECT dept_name, emp_no, full_name, salary
 FROM CTE
 WHERE rank_salary <=3
 ORDER BY 1, 4 DESC;
+
 -- answer : same! but DENSE_RANK() -> RANK()
 WITH BASIS AS 
 	(SELECT dept_name, emp_no, full_name, salary, 
@@ -128,7 +132,9 @@ FROM BASIS
 WHERE ranks <=3
 ORDER BY 1, 5;
 
+
 -- 3. Refer DEPT_EMP_SAL_V, make a query with 3 groups with salary top 10, by department.
+
 -- mine : divided the rank in CTE and NTILE in main query
 WITH CTE AS 
 	(SELECT dept_name, emp_no, full_name, salary, 
@@ -140,6 +146,7 @@ SELECT dept_name, emp_no, full_name, salary, rank_salary,
 FROM CTE
 WHERE rank_salary<=10
 ORDER BY 1, ntiles;
+
 -- answer: result is same 
 -- I put the condition in main query's `WHERE`,
 -- But this make an another CTE with BASIS and use CTE in main query simply.
@@ -159,7 +166,9 @@ SELECT dept_name, emp_no, full_name, salary,
 FROM TOP10
 ORDER BY 1, grade;
 
+
 -- 4. In BOX_OFFICE table, get 2019 release movies' monthly sale amount and a rate of change by monthly
+
 -- mine : correct. We have to solve using multiple CTE
 WITH MONTHLY_SALES AS 
 	(
