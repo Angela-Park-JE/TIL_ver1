d77b2a78b677730c85be8b37a69d3710b4d8ca66
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-11-05 21:37:02 +0900

    Update un-14-Join-SymmetricPairs.sql

--- MySQL/HackerRank/un-14-Join-SymmetricPairs.sql
+++ MySQL/HackerRank/un-14-Join-SymmetricPairs.sql
@@ -6,6 +6,32 @@ Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2
 Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1.
 """
 
+/*- MySQL : BASIS_CTE에서 rownum을 매겨서 MAIN_CTE에서 같지 않은 컬럼이 조회되도록 한후, 
+            서로가 같은 짝인 아이들이 중복으로 검색되는 것을 거르도록 x1 <= y1와 DISTINCT(x=y인 아이들 중복)를 사용한다. -*/
+WITH BASIS_CTE AS
+    (
+    SELECT x, y, 
+           ROW_NUMBER() OVER (ORDER BY x ASC) row_num
+    FROM FUNCTIONS
+    ORDER BY 1, 2
+    ),
+     MAIN_CTE AS
+    (
+    SELECT a.x x1, a.y y1, a.row_num
+    FROM BASIS_CTE a, BASIS_CTE b
+    WHERE 1=1
+      AND b.x = a.y
+      AND b.y = a.x
+      AND a.row_num != b.row_num
+    ORDER BY 1, 2
+    )
+
+SELECT DISTINCT x1, y1
+FROM MAIN_CTE
+WHERE x1 <= y1;
+
+
+
 """오답노트"""
 
 /*- MySQL : 실패한 부분. 잘못생각함. -*/
