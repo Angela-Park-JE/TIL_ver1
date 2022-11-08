2750b576b04c1f44bcfe7bfe24027e6f9eef29d7
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-11-08 21:45:13 +0900

    Update 04_LonelyInteger.py

--- python3/algorithm_HackerRank/04_LonelyInteger.py
+++ python3/algorithm_HackerRank/04_LonelyInteger.py
@@ -1,6 +1,18 @@
 """
 Prepare> Interview Preparation Kits> 1 Week Preparation Kit> Day 2> Lonely Integer
 https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem
+Given an array of integers, where all elements but one occur twice, find the unique element.
+* Example
+  `a = [1, 2, 3, 4, 3, 2, 1]`
+  The unique element is `4`.
+* Function Description
+  Complete the lonelyinteger function in the editor below.
+  lonelyinteger has the following parameter(s) : int a[n]: an array of integers
+* Returns
+  int: the element that occurs only once
+* Input Format 
+  The first line contains a single integer, `n`, the number of integers in the array.
+  The second line contains `n` space-separated integers that describe the values in `a`.
 """
 
 """ what I thought first, but it return `None` """
