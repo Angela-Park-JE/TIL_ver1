5e8bd2bb74dc511ecb2560288f2b3339c9550d2a
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-08 21:09:19 +0900

    Update X_03_Print in Reverse.py

--- python3/data_structures_HackerRank/X_03_Print in Reverse.py
+++ python3/data_structures_HackerRank/X_03_Print in Reverse.py
@@ -8,6 +8,89 @@ Given a pointer to the head of a singly-linked list, print each `data` value fro
 If the given list is empty, do not print anything.
 """
 
+""" 채 하지못한 나의  """
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+#
+# Complete the 'reversePrint' function below.
+#
+# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+# first number is number of cases
+# second number is number of first case's integer
+#
+
+def reversePrint(llist):
+    ar = []
+    while llist:
+        ar.append(llist.data)
+        llist = llist.next
+    for _ in ar[::-1]:
+        print(_)
+    return 
+        
+        
+        
+    return llist
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
+
+
 
 """현답들"""
 
