c6cb6e856f6cd7c80946093dfb44337bd40788f8
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-13 21:35:29 +0900

    Create 01_easy_627.sql

---
+++ MySQL/Leetcode/01_easy_627.sql
@@ -0,0 +1,14 @@
+"""
+627. Swap Salary
+https://leetcode.com/problems/swap-salary/description/?envType=study-plan&id=sql-i
+Write an SQL query to swap all 'f' and 'm' values (i.e., change all 'f' values to 'm' and vice versa) with a single update statement 
+  and no intermediate temporary tables.
+Note that you must write a single update statement, do not write any select statement for this problem.
+"""
+
+
+/*- mine : update 라는 것을 잊었다.-*/
+
+-- 계속 이러고 있었음
+-- SELECT id, name, CASE sex WHEN 'f' THEN 'm' WHEN 'm' THEN 'f' END sex, salary
+-- FROM SALARY;
