8f090e7eb039cda79ed46fae3ccd0d30fdab6e60
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-20 05:49:33 +0900

    Update 1_04-medium-608.sql

--- MySQL/Leetcode/1_04-medium-608.sql
+++ MySQL/Leetcode/1_04-medium-608.sql
@@ -36,7 +36,7 @@ Nodes 3, 4, and 5 are leaf nodes because they have parent nodes and they do not 
 """
 
 
-/* mine : 허허 생각보다 시간이 오래걸렸다. */
+/* mine : 허허 생각보다 시간이 오래걸렸다. 35분이상... */
 
 -- MySQL
 SELECT id, 
@@ -53,23 +53,18 @@ FROM TREE;
 
 -- 똑같이 case when을 쓰는데, 
 -- `p_id = NULL THEN 'Root'` 썼더니 Root가 안나오고 비어있었는데,
--- `p_id IS NULL THEN 'Root'` 썼더니 Root가 제대로 나왔었다! 
-
+-- `p_id IS NULL THEN 'Root'` 썼더니 Root가 제대로 나왔었다.
 SELECT id, 
        CASE WHEN p_id IS NULL THEN 'Root'
-            # WHEN p_id IN (SELECT id FROM TREE WHERE p_id IS NULL) 
-            #     AND id IN (SELECT p_id FROM TREE) THEN 'Inner'
-            WHEN id NOT IN (SELECT p_id FROM TREE) THEN 'Leaf' 
-        ELSE 'Inner' END type
-FROM TREE;
 
--- 아무튼 뭔가 단단히 잘못 생각하고 Inner 를 제대로 정리를 못하고 있다가, 어느 순간 팍 하고 떠올라버렸다
+-- 아무튼 뭔가 단단히 잘못 생각하고 Inner 를 제대로 정리를 못하고 있다가, 어느 순간 팍 하고 떠올라버렸다.
 SELECT id, 
        CASE WHEN p_id IS NULL THEN 'Root'
+       
             # WHEN p_id IS NOT NULL AND id NOT IN (SELECT DISTINCT p_id FROM TREE) THEN 'Leaf' -- 부모는 있는데 부모가 된적이 없으면 leaf 아닌가 이건 안됨
             
             # WHEN p_id IN (SELECT id FROM TREE WHERE p_id IS NULL) 
-            #     AND id IN (SELECT DISTINCT p_id FROM TREE) THEN 'Inner' -- 이게아니라
+            #     AND id IN (SELECT DISTINCT p_id FROM TREE) THEN 'Inner' -- 이게 아니라
             WHEN p_id IS NOT NULL
                 AND id IN (SELECT DISTINCT p_id FROM TREE) THEN 'Inner'   -- 이것
 
