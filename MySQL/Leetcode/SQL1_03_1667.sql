6d6df45a61605140399a298162bfd1b09cc12a7e
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-19 20:37:52 +0900

    Update SQL1_03_1667.sql

--- MySQL/Leetcode/SQL1_03_1667.sql
+++ MySQL/Leetcode/SQL1_03_1667.sql
@@ -25,17 +25,19 @@ Output:
 """
 
 /*- mine: INITCAP 은 oracle에만 있고, mysql에는 없기 떄문에 따로 지정해서 만들어야 한다. -*/
+
+-- MySQL
 SELECT user_id, CONCAT( UPPER(LEFT(name, 1)), SUBSTR(LOWER(name), 2) ) AS name
 FROM USERS
 ORDER BY user_id;
 
 
 """다른 풀이"""
-/* MySQL by.aholzmann : LEFT 와 RIGHT를 활용하여 논리적으로 작성한 모습이다. */
+-- MySQL by.aholzmann : LEFT 와 RIGHT를 활용하여 논리적으로 작성한 모습이다.
 SELECT
     user_id,
     CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name,LENGTH(name)-1))) AS name
-FROM
+FROM 
     Activity    -- 중간에 문제가 수정되었는지 이 테이블로 된 답들도 많음
 ORDER BY
     user_id ASC
