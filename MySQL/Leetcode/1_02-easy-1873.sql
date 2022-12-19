e34eb1feb03288bf0e30bbd6afee041f6dfe628c
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-20 05:04:19 +0900

    Update and rename 01_easy_1873.sql to 1_02-easy-1873.sql

--- MySQL/Leetcode/1_02-easy-1873.sql
+++ MySQL/Leetcode/1_02-easy-1873.sql
@@ -5,6 +5,28 @@ Write an SQL query to calculate the bonus of each employee.
 The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. 
 The bonus of an employee is 0 otherwise.
 Return the result table ordered by 'employee_id'.
+
+Input: 
+Employees table:
++-------------+---------+--------+
+| employee_id | name    | salary |
++-------------+---------+--------+
+| 2           | Meir    | 3000   |
+| 3           | Michael | 3800   |
+| 7           | Addilyn | 7400   |
+| 8           | Juan    | 6100   |
+| 9           | Kannon  | 7700   |
++-------------+---------+--------+
+Output: 
++-------------+-------+
+| employee_id | bonus |
++-------------+-------+
+| 2           | 0     |
+| 3           | 0     |
+| 7           | 7400  |
+| 8           | 0     |
+| 9           | 7700  |
++-------------+-------+
 """
 
 /*- mine: 아니 임플로이 아이디가 홀수이고 이름이 M으로 시작하지 않으면 보너스를 100%받는다고요? -*/
