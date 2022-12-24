f5f515183d49493efb40cc4afd762b464d8ea196
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-24 23:56:42 +0900

    Update 1_05-easy-1581.sql

--- MySQL/Leetcode/1_05-easy-1581.sql
+++ MySQL/Leetcode/1_05-easy-1581.sql
@@ -1,7 +1,8 @@
 """
 1581. Customer Who Visited but Did Not Make Any Transactions
 https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/
-Write an SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+Write an SQL query to find the IDs of the users who visited without making any transactions 
+, and the number of times they made these types of visits.
 Return the result table sorted in any order.
 
 Input: 
