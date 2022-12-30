082d56ea7b8332c46936e93138ab40c833738c61
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2022-12-30 21:19:32 +0900

    Create 1_09-easy-1393-CASE.sql

---
+++ MySQL/Leetcode/1_09-easy-1393-CASE.sql
@@ -0,0 +1,17 @@
+"""
+1393. Capital Gain/Loss
+"""
+
+
+/*- mine : CASE WHEN 으로 어떻게 꾸리는 것이 좋을까 하다가 부호를 바꾸어 놓는 서브쿼리로 임시 테이블을 만들어 조회하기로 했다. -*/
+
+-- MySQL 
+SELECT stock_name, SUM(prices) capital_gain_loss
+FROM 
+    (
+    SELECT stock_name, 
+        CASE operation WHEN 'Buy' THEN 0-price
+                    WHEN 'Sell' THEN 0+price END prices
+    FROM STOCKS
+    ) tmp
+GROUP BY 1;
