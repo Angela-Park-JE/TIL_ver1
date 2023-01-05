7a6dff1eefa89e480407fdb6a309af0913911afb
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-01-05 19:26:21 +0900

    Create 13_Staircase.py

---
+++ python3/algorithm_HackerRank/13_Staircase.py
@@ -0,0 +1,37 @@
+"""
+Prepare> Algorithms> Warmup> Staircase
+https://www.hackerrank.com/challenges/staircase/problem
+This is a staircase of size `n = 4`:
+   #
+  ##
+ ###
+####
+Its base and height are both equal to `n`. It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+Write a program that prints a staircase of size `n`.
+"""
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    for i in range(0, n):
+        i = i+1
+        print(' '*(n-i)+'#'*(i))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
