7b67e5326587452087be4ce61a58ceb33deedcb7
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-01-05 19:48:14 +0900

    Update 01_Plus Minus.py

--- python3/algorithm_HackerRank/01_Plus Minus.py
+++ python3/algorithm_HackerRank/01_Plus Minus.py
@@ -30,3 +30,38 @@ if __name__ == '__main__':
     numbers = int(input().strip())
     arr = list(map(int, input().rstrip().split()))
     plusMinus(arr)
+
+
+    
+# 나중에 문제가 또나와서 풀었는데 큰 변화는 없다 (23.01.05)
+def plusMinus(arr):
+    # Write your code here
+    p, m, z = [], [], []
+    n = len(arr)
+    for i in arr:
+        if i > 0:
+            p.append(i)
+        elif i < 0:
+            m.append(i)
+        else:
+            z.append(i)
+    numbers = len(p), len(m), len(z)
+    for i in numbers:
+        print(round(i/n, 5))
+
+
+
+"""다른 풀이"""
+
+# by.reddyjahnavi : n을 이렇게 지정해주었다. 이게맞나...? 아무튼 len()안쓰고 카운트를 올리는 형식으로, 아래 부분을 안돌려도되는 형식이다... 호오...
+n = int(input().strip())
+arr = [1 if int(temp)>0 else -1 if int(temp)<0 else 0 for temp in input().strip().split(' ') ]
+print("{0:.6f}".format(arr.count(1)/n))
+print("{0:.6f}".format(arr.count(-1)/n))
+print("{0:.6f}".format(arr.count(0)/n))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
