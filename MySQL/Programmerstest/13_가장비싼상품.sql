07ac8ec55968eac69ef1016874311f9a7c901e03
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-01-16 20:42:34 +0900

    Update 13_가장비싼상품.sql

--- MySQL/Programmerstest/13_가장비싼상품.sql
+++ MySQL/Programmerstest/13_가장비싼상품.sql
@@ -9,3 +9,23 @@ PRODUCT 테이블에서 판매 중인 상품 중 가장 높은 판매가를 출�
 -- MySQL
 SELECT MAX(price) MAX_PRICE
 FROM PRODUCT;
+
+
+
+"""
+작년에 풀었던 문제이긴 한데 그래도 이런식으로 푼 기억이 있구나 하고 적는 것
+
+최댓값 구하기
+https://school.programmers.co.kr/learn/courses/30/lessons/59415
+가장 최근에 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+"""
+
+/*- mine: 답을 두가지를 적어놨었다. MAX라서 LIMIT 을 쓸 필요가 없다. -*/
+
+-- MySQL
+-- SELECT MAX(datetime) as datetime
+-- FROM ANIMAL_INS
+-- GROUP BY datetime
+-- LIMIT 1;
+
+SELECT MAX(datetime) AS '시간' FROM ANIMAL_INS;
