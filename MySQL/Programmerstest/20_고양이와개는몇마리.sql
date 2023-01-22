f8f30bd7c54b1fd8c55df5b5d40bb33604a3d05e
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-01-23 05:42:24 +0900

    Update and rename 20_.sql to 20_고양이와개는몇마리.sql

--- MySQL/Programmerstest/20_고양이와개는몇마리.sql
+++ MySQL/Programmerstest/20_고양이와개는몇마리.sql
@@ -9,6 +9,7 @@ DATETIME	        DATETIME	FALSE
 INTAKE_CONDITION	VARCHAR(N)	FALSE
 NAME	            VARCHAR(N)	TRUE
 SEX_UPON_INTAKE	    VARCHAR(N)	FALSE
+
 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 이때 고양이를 개보다 먼저 조회해주세요.
 """
 
@@ -25,7 +26,7 @@ ORDER BY 1;
 SELECT animal_type, COUNT(*)
 FROM ANIMAL_INS
 WHERE animal_type = 'Dog' OR animal_type = 'Cat'
-GROUP BY 1
+GROUP BY 1 
 ORDER BY 1;
 
 
