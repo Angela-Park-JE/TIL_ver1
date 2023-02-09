26d723a1aad60beb2591b4bafe8c25752b59d416
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-02-10 00:36:07 +0900

    Update 15_종류별옵션검색후수구하기.sql

--- MySQL/Programmerstest/15_종류별옵션검색후수구하기.sql
+++ MySQL/Programmerstest/15_종류별옵션검색후수구하기.sql
@@ -32,3 +32,13 @@ FROM CAR_RENTAL_COMPANY_CAR
 WHERE options REGEXP '통풍시트|열선시트|가죽시트'
 GROUP BY car_type
 ORDER BY 1;
+
+
+"""잘못된 예"""
+
+-- 
+SELECT car_type, COUNT(*) CARS
+FROM CAR_RENTAL_COMPANY_CAR
+WHERE options in ('통풍시트','열선시트','가죽시트')
+GROUP BY car_type
+ORDER BY 1;
