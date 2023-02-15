07f31ef512da35d09ac986f238aba43dd8ef0651
Angela Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-02-16 03:40:25 +0900

    Update 32_조건에맞는식당아니고리뷰목록출력.sql

--- MySQL/Programmerstest/32_조건에맞는식당아니고리뷰목록출력.sql
+++ MySQL/Programmerstest/32_조건에맞는식당아니고리뷰목록출력.sql
@@ -43,3 +43,50 @@ FROM REST_REVIEW r LEFT JOIN MEMBER_PROFILE m ON r.member_id = m.member_id,
     ) tb2
 WHERE tb2.member_id = m.member_id
 ORDER BY 3 ASC, 2 ASC;
+
+
+
+"""다른 풀이"""
+
+-- MySQL
+-- https://school.programmers.co.kr/questions/44144
+SELECT 
+    A.MEMBER_NAME, 
+    B.REVIEW_TEXT, 
+    DATE_FORMAT(B.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM 
+    MEMBER_PROFILE A,
+    REST_REVIEW B,
+    (
+    SELECT
+        DISTINCT X.MEMBER_ID 
+        ,COUNT(X.REVIEW_ID)
+        OVER(PARTITION BY X.MEMBER_ID) AS CNT_RV    -- 이부분에서 GROUP BY 없이 멤버아이디별 리뷰 카운트를 했는데, 
+    FROM 
+        REST_REVIEW X
+    ORDER BY CNT_RV DESC
+    limit 1                                         -- LIMIT 으로 어떻게 여러 아이디를 출력한걸까 따로 떼서 실행해봐도 이해가 안됨
+        ) C
+WHERE 
+    A.MEMBER_ID = B.MEMBER_ID
+    AND
+    B.MEMBER_ID = C.MEMBER_ID
+ORDER BY 
+    REVIEW_DATE ASC,
+    REVIEW_TEXT ASC
+
+
+-- MySQL
+-- https://school.programmers.co.kr/questions/43590
+-- 최다 리뷰어를 추출한 후 해당 리뷰어의 id를 기반으로 원래 테이블에 조인을 하는 방식 -> 이라고 쓰셨음. 위와 방식으로보임 근데 LIMIT 은 한 줄만 뽑게 되어있을 텐데... 왜지
+SELECT toprev.MEMBER_NAME, c.REVIEW_TEXT, DATE_FORMAT(c.REVIEW_DATE,'%Y-%m-%d')
+FROM (SELECT a.MEMBER_NAME, a.MEMBER_ID, COUNT(REVIEW_ID) as count
+FROM MEMBER_PROFILE as a RIGHT JOIN REST_REVIEW as b
+    on a.MEMBER_ID = b.MEMBER_ID
+GROUP BY 2
+ORDER BY 3 DESC
+LIMIT 1) as toprev INNER JOIN REST_REVIEW as c on toprev.MEMBER_ID = c.MEMBER_ID
+ORDER BY 3 ASC, 2 ASC
+
+
+-- 
