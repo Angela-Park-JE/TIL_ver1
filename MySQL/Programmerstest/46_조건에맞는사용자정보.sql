67ef5f5342739bbabffed25982fa26510a05f6b8
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-03-15 23:51:12 +0900

    Update 46_조건에맞는사용자정보.sql

--- MySQL/Programmerstest/46_조건에맞는사용자정보.sql
+++ MySQL/Programmerstest/46_조건에맞는사용자정보.sql
@@ -1,5 +1,5 @@
 """
-조건에 맞는 사용자 정보 조회하기
+조건에 맞는 사용자 정보 조회하기 (/164670 문제와 제목 바뀜;;)
 https://school.programmers.co.kr/learn/courses/30/lessons/164668
 USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성해주세요. 
 결과는 총거래금액을 기준으로 오름차순 정렬해주세요.
