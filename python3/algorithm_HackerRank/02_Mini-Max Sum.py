183190d704aadab989e4a0d466c0b3e264ae2e41
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-03-31 23:37:19 +0900

    Update 02_Mini-Max Sum.py

--- python3/algorithm_HackerRank/02_Mini-Max Sum.py
+++ python3/algorithm_HackerRank/02_Mini-Max Sum.py
@@ -25,3 +25,16 @@ def miniMaxSum(arr):
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
     miniMaxSum(arr)
+
+    
+"""
+같은 문제인데 정말 깔끔하게 이 과정을 정리하신 분이 계셨다...
+그냥 합을 구해놓고 한쪽은 가장 큰 것을 지우고, 한쪽은 가장 작은 것을 지우면 될 일을...
+https://www.hackerrank.com/challenges/mini-max-sum/problem
+"""
+
+# by. for_andrey_luky1 - https://www.hackerrank.com/challenges/mini-max-sum/forum/comments/1266345
+# Pythonista. Sorting has O( n*log(n) ) time complexity in compare with O( n ) for min() and max() funcitons. It make sence for big data.
+def miniMaxSum(arr):
+    s = sum(arr)
+    print(f"{s-max(arr)} {s-min(arr)}")
