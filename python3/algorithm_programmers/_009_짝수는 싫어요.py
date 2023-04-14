48c6a3f3077ddcc4b0100da0790722eff80510c1
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-04-14 21:51:28 +0900

    Update _009_짝수는 싫어요.py

--- python3/algorithm_programmers/_009_짝수는 싫어요.py
+++ python3/algorithm_programmers/_009_짝수는 싫어요.py
@@ -1,5 +1,5 @@
 """
-코딩테스트 연습> 코딩테스트 입문> 짝수는 싫어요
+코딩테스트 연습> 코딩테스트 입문> 짝수는 싫어요(lv0)
 https://school.programmers.co.kr/learn/courses/30/lessons/120813
 문제 설명
   정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
