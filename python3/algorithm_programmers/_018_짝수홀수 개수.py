33c5caee05abdd1e93bff442c1575e720c6e8e61
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-04-22 20:15:01 +0900

    Update _018_짝수홀수 개수.py

--- python3/algorithm_programmers/_018_짝수홀수 개수.py
+++ python3/algorithm_programmers/_018_짝수홀수 개수.py
@@ -1,5 +1,5 @@
 """
-코딩테스트 연습> 코딩테스트 입문> 짝수 홀수 개수
+코딩테스트 연습> 코딩테스트 입문> 짝수 홀수 개수(lv0)
 https://school.programmers.co.kr/learn/courses/30/lessons/120824
 문제 설명
   정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
