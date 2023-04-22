ac08871775bf404ae389ea3fe88222a3b5580748
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-04-22 20:15:38 +0900

    Update _019_특정문자 제거하기.py

--- python3/algorithm_programmers/_019_특정문자 제거하기.py
+++ python3/algorithm_programmers/_019_특정문자 제거하기.py
@@ -1,5 +1,5 @@
 """
-코딩테스트 연습> 코딩테스트 입문> 특정 문자 제거하기
+코딩테스트 연습> 코딩테스트 입문> 특정 문자 제거하기(lv0)
 https://school.programmers.co.kr/learn/courses/30/lessons/120826
 문제 설명
   문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
