be5d959521354d647652c73616db225d57f27b20
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-04-24 21:12:20 +0900

    Update _022_외계행성의 나이.py

--- python3/algorithm_programmers/_022_외계행성의 나이.py
+++ python3/algorithm_programmers/_022_외계행성의 나이.py
@@ -29,3 +29,58 @@ def solution(age):
         a = al[int(i)]
         answer = answer + a
     return answer
+  
+"""
+테스트 1 〉	통과 (0.65ms, 10.4MB)
+테스트 2 〉	통과 (47.23ms, 10.4MB)
+테스트 3 〉	통과 (0.70ms, 10.6MB)
+테스트 4 〉	통과 (1.05ms, 10.4MB)
+테스트 5 〉	통과 (0.71ms, 10.5MB)
+테스트 6 〉	통과 (1.05ms, 10.4MB)
+"""
+
+
+# 딕셔너리로 바꾼 형태이다. 확실히 전체적으로 속도가 빠르다.
+def solution(age):
+    answer = ''
+    from string import ascii_lowercase
+    dicts = dict(zip([n for n in range(0, 10)], list(ascii_lowercase)[:10]))
+    for i in list(str(age)):
+        answer = answer + dicts[int(i)]
+    return answer
+
+"""
+테스트 1 〉	통과 (0.70ms, 10.4MB)
+테스트 2 〉	통과 (0.67ms, 10.4MB)
+테스트 3 〉	통과 (0.65ms, 10.4MB)
+테스트 4 〉	통과 (0.66ms, 10.4MB)
+테스트 5 〉	통과 (1.14ms, 10.4MB)
+테스트 6 〉	통과 (0.69ms, 10.5MB)
+"""
+
+
+
+"""다른 풀이"""
+
+# by. 봉글봉글 , 서병일 , 김진영 , le2dy 외 66 명
+# age에서 가져온 숫자로 알파벳을 가져온다. 유니코드를 활용한 코드. 
+def solution(age):
+
+    return ''.join([chr(int(i)+97) for i in str(age)])
+
+
+# by. Sehyeon Jeong
+# string과 함께했지만 훨씬 깔끔하게, 딕셔너리 없이 map과 람다로 해결했다. 와.
+import string
+
+def solution(age):
+    return "".join(map(lambda v: string.ascii_lowercase[int(v)], str(age)))
+
+
+# by.김종도
+# 훨씬 간단하게 딕셔너리를 만들고, 리스트 컴프리헨션 안에 for를 넣은뒤 바로 join으로 답을 내놓는 이상적인 답안이다.
+# 게다가 string 라이브러리 없이, ord()라는 함수를 이용하여 'a'부터 시작하는 알파벳 리스트를 만들었다... 대단
+def solution(age):
+    answer = ''
+    char_num = {str(i): chr(ord('a') + i) for i in range(26)}
+    return ''.join([char_num[x] for x in str(age)])
