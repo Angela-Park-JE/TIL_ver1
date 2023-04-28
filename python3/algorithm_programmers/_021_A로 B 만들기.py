09d63cb14b0f6f702db9227d0ca2b30f8109d77d
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-04-28 13:23:33 +0900

    Update _021_A로 B 만들기.py

--- python3/algorithm_programmers/_021_A로 B 만들기.py
+++ python3/algorithm_programmers/_021_A로 B 만들기.py
@@ -1,5 +1,5 @@
 """
-코딩테스트 연습> 코딩테스트 입문> A로 B 만들기
+코딩테스트 연습> 코딩테스트 입문> A로 B 만들기(lv0)
 https://school.programmers.co.kr/learn/courses/30/lessons/120886
 문제 설명
   문자열 before와 after가 매개변수로 주어질 때, before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.
