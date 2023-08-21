e85edacb0b6b1d1ce8cd4f94d8cfa35dc899dd12
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-08-21 21:04:29 +0900

    Update 30_취소되지않은진료.sql

--- MySQL/Programmerstest/30_취소되지않은진료.sql
+++ MySQL/Programmerstest/30_취소되지않은진료.sql
@@ -23,3 +23,14 @@ WHERE a.pt_no = p.pt_no AND a.mddr_id = d.dr_id
   AND apnt_cncl_yn != 'Y'
   AND a.mcdp_cd = 'CS'
 ORDER BY a.apnt_ymd ASC;
+
+
+
+-- 복습
+-- 230820: DATE 안하고 날짜랑 비교하고있으니 뜨지 않았다. 신기하게도 완전히 똑같다.
+SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
+  FROM APPOINTMENT a, PATIENT p, DOCTOR d
+ WHERE p.pt_no = a.pt_no AND d.dr_id = a.mddr_id
+   AND DATE(a.apnt_ymd) = '2022-04-13'
+   AND a.mcdp_cd = 'CS' AND a.apnt_cncl_yn = 'N'
+ ORDER BY a.apnt_ymd ASC;
