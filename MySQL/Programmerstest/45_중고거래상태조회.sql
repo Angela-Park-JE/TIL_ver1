c43e5bac620749c900f3f9800053e426234d818a
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-08-24 21:09:13 +0900

    Update 45_중고거래상태조회.sql

--- MySQL/Programmerstest/45_중고거래상태조회.sql
+++ MySQL/Programmerstest/45_중고거래상태조회.sql
@@ -21,4 +21,13 @@ SELECT board_id, writer_id, title, price,
 
 
 -- 복습
--- 
+-- 230824: 어려울 것 없다
+SELECT board_id, writer_id, title, price, 
+       CASE status 
+           WHEN 'SALE' THEN '판매중' 
+           WHEN 'RESERVED' THEN '예약중' 
+           WHEN 'DONE' THEN '거래완료' 
+       END AS '거래상태'
+FROM USED_GOODS_BOARD
+WHERE created_date = '2022-10-05'
+ORDER BY 1 DESC;
