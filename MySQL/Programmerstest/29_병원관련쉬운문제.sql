f9aa5e1eb68b670e6d6fcc321e02fe6e571e167e
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-09-23 04:46:54 +0900

    Update 29_병원관련쉬운문제.sql

--- MySQL/Programmerstest/29_병원관련쉬운문제.sql
+++ MySQL/Programmerstest/29_병원관련쉬운문제.sql
@@ -24,7 +24,7 @@ https://school.programmers.co.kr/learn/courses/30/lessons/132201
 PATIENT 테이블은 다음과 같으며 PT_NO, PT_NAME, GEND_CD, AGE, TLNO는 각각 환자번호, 환자이름, 성별코드, 나이, 전화번호를 의미합니다.
 PATIENT 테이블에서 12세 이하인 여자환자의 환자이름, 환자번호, 성별코드, 나이, 전화번호를 조회하는 SQL문을 작성해주세요. 
 이때 전화번호가 없는 경우, 'NONE'으로 출력시켜 주시고 결과는 나이를 기준으로 내림차순 정렬하고, 나이 같다면 환자이름을 기준으로 오름차순 정렬해주세요.
-"""
+""" 
 
 
 /*- mine : IFNULL 만 알면 쉬워지는 문제이다. -*/
