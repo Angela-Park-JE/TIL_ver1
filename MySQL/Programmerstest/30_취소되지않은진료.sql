2cf8128bf49f1ed9b451ad21021699546c87175c
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-09-26 23:21:53 +0900

    Update 30_취소되지않은진료.sql

--- MySQL/Programmerstest/30_취소되지않은진료.sql
+++ MySQL/Programmerstest/30_취소되지않은진료.sql
@@ -34,3 +34,7 @@ SELECT a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
    AND DATE(a.apnt_ymd) = '2022-04-13'
    AND a.mcdp_cd = 'CS' AND a.apnt_cncl_yn = 'N'
  ORDER BY a.apnt_ymd ASC;
+
+
+
+-- 복습
