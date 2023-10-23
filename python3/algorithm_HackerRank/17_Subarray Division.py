272045fc569dab5675722ebd4b5048bc48ca39ae
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-10-23 20:34:28 +0900

    Update 17_Subarray Division.py

--- python3/algorithm_HackerRank/17_Subarray Division.py
+++ python3/algorithm_HackerRank/17_Subarray Division.py
@@ -2,15 +2,17 @@
 Prepare> Algorithms> Implementation> Subarray Division
 https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true
 Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
-Lily decides to share a contiguous segment of the bar selected such that:
+> Lily decides to share a contiguous segment of the bar selected such that:
   The length of the segment matches Ron's birth month, and,
   The sum of the integers on the squares is equal to his birth day.
 Determine how many ways she can divide the chocolate.
-Function Description
+> Function Description
   `birthday()` has the following parameter(s):
   int s[n]: the numbers on each of the squares of chocolate
   int d: Ron's birth day
   int m: Ron's birth month
+> Returns
+  int: the number of ways the bar can be divided
 """
 
 
