70e5bbd086cec2d28234f2ee957bb55dcbb88258
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-10-23 20:33:57 +0900

    Update and rename X-17.py to 17_Subarray Division.py

--- python3/algorithm_HackerRank/17_Subarray Division.py
+++ python3/algorithm_HackerRank/17_Subarray Division.py
@@ -33,16 +33,36 @@ import sys
 #  3. INTEGER m
 #
 
+"""
 # 231022
 def birthday(s, d, m):
+    # Write your code here
+
     # the choco length is month m : 초코나눈 길이는 m이고
     # the choco square number's sum is day d : 초코에 그려진 숫자 합은 d인 케이스를 찾아야 한다.
     
     for i in range(len(s)-(m-1)):
         # repeate m times s[i+ ...]
         # s[i] m부터 번만큼 하나씩 인덱스 이동해서 더한 값이 choco인데 이게 d와 같으면 count를 하나씩 올리는 식으로 하면 된다.
-        choco = s[i]+s[i+]
-    # Write your code here
+        choco = s[i]+s[i+...] # repeate m times s[i+ ...]
+"""
+
+"""# 231023"""
+def birthday(s, d, m):
+
+    cnt = 0
+    for i in range(len(s)-(m-1)):
+        choco_num = 0
+        choco_num += s[i]
+        for times in range(1, m):
+            choco_num += s[i+times]
+        if choco_num == d:
+            cnt += 1
+        else:
+            continue
+    
+    return cnt
+
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
