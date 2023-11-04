8274284109d20d157ffc51d2ebaeb3fb159ca4e6
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-11-04 23:35:33 +0900

    Update 39_레벨파이브상품구매한사람들.sql

--- MySQL/Programmerstest/39_레벨파이브상품구매한사람들.sql
+++ MySQL/Programmerstest/39_레벨파이브상품구매한사람들.sql
@@ -1,6 +1,6 @@
 """
 상품을 구매한 회원 비율 구하기
-https://school.programmers.co.kr/learn/courses/30/lesso/131534
+https://school.programmers.co.kr/learn/courses/30/lessons/131534
 USER_INFO 테이블과 ONLINE_SALE 테이블에서 2021년에 가입한 전체 회원들 중, 상품을 구매한 회원수와 상품을 구매한 회원의 비율
 (=2021년에 가입한 회원 중 상품을 구매한 회원수 / 2021년에 가입한 전체 회원 수)을 년, 월 별로 출력하는 SQL문을 작성해주세요. 
 상품을 구매한 회원의 비율은 소수점 두번째자리에서 반올림하고, 전체 결과는 년을 기준으로 오름차순 정렬해주시고 년이 같다면 월을 기준으로 오름차순 정렬해주세요.
