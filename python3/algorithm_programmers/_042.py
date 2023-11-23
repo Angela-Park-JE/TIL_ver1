44a76eeae403a0801db12184858a8e14365fb4c9
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-11-23 21:07:52 +0900

    Update _042.py

--- python3/algorithm_programmers/_042.py
+++ python3/algorithm_programmers/_042.py
@@ -3,8 +3,15 @@
 
 # https://school.programmers.co.kr/learn/courses/30/lessons/42576
 
+
+
+
+"""오답노트"""
+# 231118: dictionary로 키를 사람이름으로 하려 했으나 생각해보니 잘못됐다.
+# 동명이인의 경우는 수가 맞는지 확인해야한다. 
 def solution(participant, completion):
     dict_p = dict.fromkeys(participant, 0)
+    answer = 0
     for i in completion:
         dict_p[i] += 1
     for j in dict_p:
@@ -13,3 +20,10 @@ def solution(participant, completion):
         else:
             continue
     return answer
+
+# 231123: participant에서 completion에 있는 것들을 하나씩 빼기로 한다.
+def solution(participant, completion):
+    for i in completion:
+        participant.remove(i)
+    return participant[0]
+    # 그러나 효율성 테스트에서 시간 초과가 걸리고 만다.
