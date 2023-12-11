2d83fdf2799f503d9611b4ed710aea13e90663b8
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2023-12-11 18:07:25 +0900

    Update _044_a와 b출력하기.py

--- python3/algorithm_programmers/_044_a와 b출력하기.py
+++ python3/algorithm_programmers/_044_a와 b출력하기.py
@@ -17,3 +17,28 @@ https://school.programmers.co.kr/learn/courses/30/lessons/181951
 a, b = map(int, input().strip().split(' '))
 # your code
 print(f'a = {a}\nb = {b}')
+
+
+
+
+"""
+코딩테스트 연습> 코딩 기초 트레이닝> 문자열 반복해서 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181950
+문자열 str과 정수 n이 주어집니다.
+str이 n번 반복된 문자열을 만들어 출력하는 코드를 작성해 보세요.
+제한사항
+  1 ≤ str의 길이 ≤ 10
+  1 ≤ n ≤ 5
+입력 #1
+  string 5
+출력 #1
+  stringstringstringstringstring
+"""
+
+
+
+# 23.12.11
+a, b = input().strip().split(' ')
+b = int(b)
+# your code
+print(a*b)
