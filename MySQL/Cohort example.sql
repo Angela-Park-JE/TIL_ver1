8bfd359a494c88473f99f6268613b99a677878f1
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-01-05 20:40:43 +0900

    Update and rename cohort example.sql to Cohort example.sql

--- MySQL/Cohort example.sql
+++ MySQL/Cohort example.sql
@@ -1,20 +1,20 @@
-/*- 프리온보딩1월챌린지-2강에서 (240104)
+/*- 프리온보딩데이터1월챌린지-2강에서 (240104)
 with로 각 퍼널별 id개수를 세 놓은 다음, 메인 쿼리에서 COUNTIF로 NULL이 아닌 것을 세는 방식으로 할 수 있다. 
-월요일을 시작으로 주 별 퍼널분석을 한다고 생각해보자. -*/
+월요일을 시작으로 주 별 퍼널 분석을 한다고 생각해보자. (이커머스라고 가정하기로 변수명 변경함) -*/
 
- WITH prepage AS
+ WITH prepage AS -- 물건 정보를 보기 위해 거쳐야 하는 페이지
       (
       SELECT DATE(DATE_TRUNC(time, WEEK(MONDAY))) AS week, ses_id
       FROM db2024.LOG_DATA
-      WHERE event_name in ('recommend_v', 'cate_v', 'search_v', 'liked_v')
+      WHERE event_name in ('recommend_v', 'cate_v', 'search_v', 'liked_v', 'newsletter_v', 'events_v', 'cart_v')
       ),
-      productpage AS 
+      productpage AS -- 물건 정보 페이지
       (
       SELECT DATE(DATE_TRUNC(timestamp, WEEK(MONDAY))) AS week, ses_id
       FROM db2024.LOG_DATA
       WHERE event_name = 'product_view'
       ),
-      cartpage AS
+      cartpage AS -- 장바구니 페이지: 물건 정보에서 장바구니에 담았을 때 이동한다. 구매하려면 장바구니를 거쳐야 한다고 해보자.
       (
       SELECT DATE(DATE_TRUNC(timestamp, WEEK(MONDAY))) AS week, ses_id
       FROM db2024.LOG_DATA
