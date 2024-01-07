b901cf1be5ddeb31c0adb9b961821e272b2970a8
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-01-07 23:42:32 +0900

    Update json in bigquery.sql

--- MySQL/json in bigquery.sql
+++ MySQL/json in bigquery.sql
@@ -1,1 +1,6 @@
-
+SELECT id
+  FROM DATA.LOG_DATA
+ WHERE 1=1
+  AND event_name = 'category_view'
+  -- AND JSON_QUERY(event_property, '$.use_search') = 'used'; 
+  AND JSON_EXTRACT_SCALAR(event_property,'$.use_search')='used';
