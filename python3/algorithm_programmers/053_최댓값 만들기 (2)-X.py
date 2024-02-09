d0ad3a008375d626fed5d19f60fab299709e54b8
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-02-10 03:54:39 +0900

    Update 053_최댓값 만들기 (2)-X.py

--- python3/algorithm_programmers/053_최댓값 만들기 (2)-X.py
+++ python3/algorithm_programmers/053_최댓값 만들기 (2)-X.py
@@ -10,7 +10,17 @@ https://school.programmers.co.kr/learn/courses/30/lessons/120862#
 
 
 
-# 오답노트 #
+# 24.02.10: 결국 최대가 음수라면 0이 최댓값으로 배출되어서는 아니된다! 그리고 변수 정의 안하려고 안간힘 썼다.
+def solution(numbers):
+    # 음수 양수 둘 뿐이라면 답은 음수가 된다.
+    if (len(numbers) == 2)&(numbers[0] * numbers[1] <0):
+        return numbers[0]*numbers[1]
+    else:   # 일반적 양수일때, 음수끼리의 곱이 클떄, 0이 최대일때
+        return max([sorted(numbers)[-1] * sorted(numbers)[-2], sorted(numbers)[0] * sorted(numbers)[1], 0])
+
+
+
+"""오답노트"""
 
 # 24.01.24: 가볍게 풀려고 시작했는데 생각보다 안풀렸다. 일일히 해보는 방식이 싫어서 했는데 케이스를 나누는게 잘못된 것 같다.
 def solution(numbers):
@@ -30,5 +40,4 @@ def solution(numbers):
             s2 = s1         # [양수, 음수]
     
     # 생각해보니 [320, 0, -2, -4] 뭐 이런 거있을 수 있을텐데
-    
     return max([s1, s2, 0])
