54b54e5f2bda8def1d7b8e7cd2b27ccb6c93c257
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-03-03 21:15:54 +0900

    Update 061_세 개의 구분자-X.py

--- python3/algorithm_programmers/061_세 개의 구분자-X.py
+++ python3/algorithm_programmers/061_세 개의 구분자-X.py
@@ -15,6 +15,8 @@ https://school.programmers.co.kr/learn/courses/30/lessons/181862
 
 # 24.03.03: 드디어 되었다. abc를 모두 한 가지 구분자로 바꾼다음 한번에 자른 뒤 빈문자열 버리고 빈건지 아닌지 고르기!
 def solution(myStr):
+    # 이 부분은 아래로 대체 될 수 있다. ㅎㅎ
+    # strlist = myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split(' ')
     for i in range(len(myStr)):
         if myStr[i] in ['a', 'b', 'c']:
             myStr = myStr.replace(myStr[i], '.')
@@ -25,6 +27,25 @@ def solution(myStr):
         return strlist
     else:
         return ['EMPTY']
+
+
+
+"""다른 답"""
+# 나는 split을 연이어 쓸 순 없지만 replace를 연이어서는 쓸 수 있다는 점을 간과했었다. 
+# 김종도 , 김은성 , 탈퇴한 사용자
+def solution(myStr):
+    answer = [x for x in myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split() if x]
+    return answer if answer else ['EMPTY']
+
+# 이게 정답이겠다 싶었다.
+# LeeShinwon
+import re
+def solution(myStr):
+    answer = [m for m in re.split('a|b|c',myStr) if m]
+    if len(answer)==0:
+        answer=["EMPTY"]
+
+    return answer
 
 
 
