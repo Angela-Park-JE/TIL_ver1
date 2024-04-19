cc44569bd9c3c1b0cd40f9721e02d2ddc12fa664
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-04-19 21:03:32 +0900

    Update 53_대장균들의 자식의 수 구하기.sql

--- MySQL/Programmerstest/53_대장균들의 자식의 수 구하기.sql
+++ MySQL/Programmerstest/53_대장균들의 자식의 수 구하기.sql
@@ -16,3 +16,17 @@ SELECT  e1.id
                ON e1.id = e2.parent_id
  GROUP  BY e1.id
  ORDER  BY 1;
+
+-- 한 가지 코멘트를 더 하자면, 자식이 없다면 = 즉 parent_id를 기준으로 id의 개수를 셌을 경우 parent_id 가 NULL인 행들이 생긴다.
+-- 이 부분이 자연스럽게 해결될 수 있는 left join을 사용하거나 방금 전의 케이스를 서브쿼리로 넣어 사용할 수 있다. 서브쿼리로 넣고 조인을 시킨다.
+SELECT  e.id AS id
+      , CASE WHEN cnt IS NULL THEN 0 ELSE cnt END AS CHILD_COUNT -- 부모가 없어서 cnt가 0인 e테이블의 모든 id 정보
+  FROM  ECOLI_DATA e
+        LEFT JOIN 
+        (
+    SELECT  parent_id, COUNT(id) AS cnt
+      FROM  ECOLI_DATA
+     GROUP  BY 1
+        ) tmp 
+               ON e.id = tmp.parent_id -- subquery's NULL will be deleted
+  ORDER  BY 1;
