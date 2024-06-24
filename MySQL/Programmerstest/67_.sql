d66a0202ec29a4d8b361777bb4ee342093a8a15f
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-06-24 23:45:58 +0900

    Update 67_부모의 형질을 모두 가지는 대장균 찾기.sql

--- MySQL/Programmerstest/67_.sql
+++ MySQL/Programmerstest/67_.sql
@@ -1,1 +1,10 @@
 
+
+
+/*- 오답노트 -*/
+-- 240624: 처음에는 이진수라는 문제를 제대로 읽지 않고 썼다. 그럼 그렇지! 예시 데이터만 봤을 때도 아님을 느꼈는데!
+SELECT e1.id 
+  FROM ECOLI_DATA e1, ECOLI_DATA e2 
+ WHERE 1=1
+   AND e1.parent_id = e2.id 
+   AND e1.genotype = e2.genotype;
