2b12d021354b3f146b1041f39c34d3593dac6317
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-07-01 23:36:25 +0900

    Update 67_.sql

--- MySQL/Programmerstest/67_.sql
+++ MySQL/Programmerstest/67_.sql
@@ -1,4 +1,12 @@
-/*- Binary with SQL -*/
+/*- Binary with SQL 
+코딩테스트 연습> SELECT> 부모의 형질을 모두 가지는 대장균 찾기
+https://school.programmers.co.kr/learn/courses/30/lessons/301647
+문제
+  부모의 형질을 모두 보유한 대장균의 ID(ID), 대장균의 형질(GENOTYPE), 부모 대장균의 형질(PARENT_GENOTYPE)을 출력하는 SQL 문을 작성해주세요. 
+  이때 결과는 ID에 대해 오름차순 정렬해주세요.
+-*/
+
+
 
 
 /*- 오답노트 -*/
