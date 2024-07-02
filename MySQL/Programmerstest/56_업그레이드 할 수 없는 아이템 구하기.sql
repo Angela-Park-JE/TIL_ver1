f47afbdccdfa8e359b85a44523cbcafbfb1e476f
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-07-02 21:15:54 +0900

    Update 56_업그레이드 할 수 없는 아이템 구하기.sql

--- MySQL/Programmerstest/56_업그레이드 할 수 없는 아이템 구하기.sql
+++ MySQL/Programmerstest/56_업그레이드 할 수 없는 아이템 구하기.sql
@@ -1,10 +1,9 @@
-"""
+/*-
 코딩테스트 연습> IS NULL> 업그레이드 할 수 없는 아이템 구하기
 https://school.programmers.co.kr/learn/courses/30/lessons/273712
   더 이상 업그레이드할 수 없는 아이템의 아이템 ID(ITEM_ID), 아이템 명(ITEM_NAME), 아이템의 희귀도(RARITY)를 출력하는 SQL 문을 작성해 주세요. 
   이때 결과는 아이템 ID를 기준으로 내림차순 정렬해 주세요.
-"""
-
+-*/
 
 
 -- 240425 : parent_id가 되지 않는 item을 찾아라!
@@ -17,4 +16,3 @@ SELECT  i.item_id AS item_id
             -- parent가 즉 하위가 되는거라 의미적으로 생각하다보면 사고하는데 좀 헷갈릴 수 있다.
  WHERE  t.item_id IS NULL
  ORDER  BY  1 DESC;
-  
