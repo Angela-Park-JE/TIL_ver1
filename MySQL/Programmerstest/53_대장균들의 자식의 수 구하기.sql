b49e839505b08643fbab00193489d8dae17a41fd
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-07-11 02:18:37 +0900

    Update 53_대장균들의 자식의 수 구하기.sql

--- MySQL/Programmerstest/53_대장균들의 자식의 수 구하기.sql
+++ MySQL/Programmerstest/53_대장균들의 자식의 수 구하기.sql
@@ -1,11 +1,10 @@
-"""
+/*-
 코딩테스트 연습> SELECT> 대장균들의 자식의 수 구하기
 https://school.programmers.co.kr/learn/courses/30/lessons/299305
   대장균 개체의 ID(ID)와 자식의 수(CHILD_COUNT)를 출력하는 SQL 문을 작성해주세요. 
   자식이 없다면 자식의 수는 0으로 출력해주세요. 
   이때 결과는 개체의 ID 에 대해 오름차순 정렬해주세요.
-"""
-
+-*/
 
 
 -- 240419: 어려울 것 없는 문제이긴 하나 테이블이 많아지면 (grandson을 구해야 한다던가..) 헷갈리기 쉬운 문제
@@ -24,9 +23,9 @@ SELECT  e.id AS id
   FROM  ECOLI_DATA e
         LEFT JOIN 
         (
-    SELECT  parent_id, COUNT(id) AS cnt
-      FROM  ECOLI_DATA
-     GROUP  BY 1
+          SELECT  parent_id, COUNT(id) AS cnt
+            FROM  ECOLI_DATA
+           GROUP  BY 1
         ) tmp 
                ON e.id = tmp.parent_id -- subquery's NULL will be deleted
   ORDER  BY 1;
