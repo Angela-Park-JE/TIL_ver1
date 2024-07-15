45758689703b93d13e675d7fabfd31b572f8f562
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-07-16 01:03:27 +0900

    Create 068_자릿수 더하기.py

---
+++ python3/algorithm_programmers/068_자릿수 더하기.py
@@ -0,0 +1,9 @@
+# 코딩테스트 연습> 코딩테스트 입문> 자릿수 더하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/120906?language=python3
+# 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 return하도록 solution 함수를 완성해주세요
+
+
+# 240715
+def solution(n):
+    answer = sum([int(i) for i in str(n)])
+    return answer
