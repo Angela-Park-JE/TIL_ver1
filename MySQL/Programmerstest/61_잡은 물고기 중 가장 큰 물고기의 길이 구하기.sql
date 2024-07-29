55ac118a9cc527844dd5276493646d46a5e47377
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-07-29 23:36:25 +0900

    Update 61_잡은 물고기 중 가장 큰 물고기의 길이 구하기.sql

--- MySQL/Programmerstest/61_잡은 물고기 중 가장 큰 물고기의 길이 구하기.sql
+++ MySQL/Programmerstest/61_잡은 물고기 중 가장 큰 물고기의 길이 구하기.sql
@@ -1,10 +1,9 @@
-"""
+/*-
 코딩테스트 연습> SUM, MAX, MIN> 잡은 물고기 중 가장 큰 물고기의 길이 구하기
 https://school.programmers.co.kr/learn/courses/30/lessons/298515
   FISH_INFO 테이블에서 잡은 물고기 중 가장 큰 물고기의 길이를 'cm' 를 붙여 출력하는 SQL 문을 작성해주세요.
   이 때 컬럼명은 'MAX_LENGTH' 로 지정해주세요.
-"""
-
+-*/
 
 
 -- 240508: 간단하지만 ORDER BY와 LIMIT을 빼놓으면 안된다.
