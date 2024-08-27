52185104a1df33469f54cc2143284fda9edf3660
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-08-27 20:05:50 +0900

    Create [2] Basic JOIN - 1280.sql

---
+++ MySQL/Leetcode/SQL50/[2] Basic JOIN - 1280.sql
@@ -0,0 +1,32 @@
+/*-
+1280. Students and Examinations
+https://leetcode.com/problems/students-and-examinations/description/?envType=study-plan-v2&envId=top-sql-50
+  Write a solution to find the number of times each student attended each exam.
+  Return the result table ordered by student_id and subject_name.
+  Table: Students                Table: Subjects
+  +---------------+---------+    +--------------+---------+
+  | Column Name   | Type    |    | Column Name  | Type    |
+  +---------------+---------+    +--------------+---------+
+  | student_id    | int     |    | subject_name | varchar |
+  | student_name  | varchar |    +--------------+---------+
+  +---------------+---------+
+  Table: Examinations
+  +--------------+---------+
+  | Column Name  | Type    |
+  +--------------+---------+
+  | student_id   | int     |
+  | subject_name | varchar |
+  +--------------+---------+
+-*/
+
+
+/* 오답노트 */
+
+-- 240827: 이것은 0회인 학생-과목에 대해 출력하지 않아 오답이다.
+SELECT  s.student_id
+      , s.student_name
+      , e.subject_name
+      , COUNT(e.student_id) AS attended_exams
+  FROM  STUDENTS s RIGHT JOIN EXAMINATIONS e
+        ON s.student_id = e.student_id
+ GROUP  BY s.student_id, s.student_name, e.subject_name
