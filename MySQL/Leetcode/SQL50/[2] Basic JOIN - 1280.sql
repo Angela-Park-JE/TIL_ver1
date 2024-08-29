fc48261da56e7f81e23697f317d3400564727dea
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-08-29 23:35:44 +0900

    Update [2] Basic JOIN - 1280.sql

--- MySQL/Leetcode/SQL50/[2] Basic JOIN - 1280.sql
+++ MySQL/Leetcode/SQL50/[2] Basic JOIN - 1280.sql
@@ -29,4 +29,16 @@ SELECT  s.student_id
       , COUNT(e.student_id) AS attended_exams
   FROM  STUDENTS s RIGHT JOIN EXAMINATIONS e
         ON s.student_id = e.student_id
- GROUP  BY s.student_id, s.student_name, e.subject_name
+ GROUP  BY s.student_id, s.student_name, e.subject_name;
+
+-- 240829: FULL JOIN을 쉽게 사용하기 위해 Oracle로 넘어왔으나 student_id 부터 null이 떠버렸다.
+SELECT  DISTINCT e.student_id
+      , s.student_name
+      , e.subject_name
+      , COUNT(e.student_id) AS attended_exams
+  FROM  EXAMINATIONS e FULL JOIN STUDENTS s
+        ON e.student_id = s.student_id
+        FULL JOIN SUBJECTS sub
+        ON e.subject_name = sub.subject_name
+ GROUP  BY e.student_id, s.student_name, e.subject_name
+ ORDER  BY 1, 3;
