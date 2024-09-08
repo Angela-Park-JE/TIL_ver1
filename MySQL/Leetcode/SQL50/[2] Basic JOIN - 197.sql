9992f6cc210b636219b8f433c3bf83efe79c922f
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-09-08 20:14:19 +0900

    Update and rename X_[2] Basic JOIN - 197.sql to [2] Basic JOIN - 197.sql

--- MySQL/Leetcode/SQL50/[2] Basic JOIN - 197.sql
+++ MySQL/Leetcode/SQL50/[2] Basic JOIN - 197.sql
@@ -14,11 +14,21 @@ https://leetcode.com/problems/rising-temperature/?envType=study-plan-v2&envId=to
 -*/
 
 
+-- 240908: DATEDIFF는 맞고 날짜 연산은 틀리다
+SELECT  w2.id AS id
+  FROM  WEATHER w1 RIGHT JOIN WEATHER w2  -- 어제 날짜 기록이 있는 데이터 기준
+        -- ON w1.recordDate - 1 = w2.recordDate 
+        ON DATEDIFF(w2.recordDate, w1.recordDate) = 1
+ WHERE  w1.temperature < w2.temperature
+ ORDER  BY w1.id;
+
+
+
+/* 오답노트 */
+
 -- 240812: test case 한 개가 넘어가질 않는다.
 SELECT  w2.id -- 오늘
   FROM  WEATHER w1 -- 어제 
         LEFT JOIN WEATHER w2    -- RIGHT 조인도 되지 않음
         ON w1.recordDate + 1 = w2.recordDate 
  WHERE  w1.temperature < w2.temperature;
-
-
