941bf6b4ef9369415dfe1ae25e2794f3fa5eeb44
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-09-17 00:03:50 +0900

    Update [5] Advanced Select and Joins - 1731.sql

--- MySQL/Leetcode/SQL50/[5] Advanced Select and Joins - 1731.sql
+++ MySQL/Leetcode/SQL50/[5] Advanced Select and Joins - 1731.sql
@@ -18,4 +18,15 @@ https://leetcode.com/problems/the-number-of-employees-which-report-to-each-emplo
 */
 
 
--- 240916
+-- 240916: 먼저 원래 테이블에 테이블을 붙여 지목당한 사람별로 groupby 후 그들의 name과 나이를 센다.
+-- 마지막에 두번째 줄 쓰는데 이게 됐었지 아마? 이렇게 셀픞조인 조회될거야 더듬더듬 해서 했다.
+-- 문제는 그 즈음 해서 아니 나 왜 메인쿼리에 두 번 조인했나-했다.
+SELECT  e1.reports_to AS employee_id
+      , (SELECT e3.name FROM EMPLOYEES e3 WHERE e3.employee_id = e1.reports_to) AS name
+      , COUNT(e2.employee_id) AS reports_count
+      , ROUND(AVG(e1.age)) AS average_age
+  FROM  EMPLOYEES e1 LEFT JOIN EMPLOYEES e2 
+        ON e1.reports_to = e2.employee_id
+ GROUP  BY e1.reports_to
+HAVING  COUNT(e1.reports_to)>=1
+ ORDER  BY 1;
