6a0712450ff27c28daf4f3bf68f2aad72c071943
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-09-21 20:35:58 +0900

    Update [5] Advanced Select and Joins - 1789.sql

--- MySQL/Leetcode/SQL50/[5] Advanced Select and Joins - 1789.sql
+++ MySQL/Leetcode/SQL50/[5] Advanced Select and Joins - 1789.sql
@@ -20,7 +20,7 @@ https://leetcode.com/problems/primary-department-for-each-employee/description/?
 
 -- 240921: WINDOW 함수로 COUNT하는 방식으로 이게 primary인지 아닌지 체크하도록 만들었다.
 -- 1개면 primary, 2개 이상이면 'Y'가 있는 것이 primary로 department_id를 남기고, 
--- 그 외는 0으로 처리한 다음 department_id 가 0인 것만 골라내버렸다.
+-- 그 외는 0으로 처리한 다음 department_id 가 0이 아닌 것만 걸러지도록 했다.
 -- (1) 만약 department_id가 0이나 NULL 있는 회사 데이터라면 기존 데이터 형식이 아닌 (e.g. VARCHAR) 임의의 데이터를 넣는 것도 방법이고,
 -- (2) 아니면 아예 새 컬럼을 만들거나 COUNT(primary_flag)가 1개인 경우 'Y'가 되도록 primary_flag 컬럼을 새로 수정한 뒤 그것을 기준으로 필터링 해도 좋다.
 -- 처음엔 5% beats를 기록했으나 다시 제출하니 89%가 나온다. 역시 믿을게 못되는 leetcode runtime!
