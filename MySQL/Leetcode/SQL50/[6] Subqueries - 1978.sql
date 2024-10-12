0d66590bc460aa742987f28e56b2a89d49abde81
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-10-12 23:41:21 +0900

    Update [6] Subqueries - 1978.sql

--- MySQL/Leetcode/SQL50/[6] Subqueries - 1978.sql
+++ MySQL/Leetcode/SQL50/[6] Subqueries - 1978.sql
@@ -1,9 +1,10 @@
 /*
 1978. Employees Whose Manager Left the Company
-Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. 
-When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
-Return the result table ordered by employee_id.
-The result format is in the following example.
+https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50
+  Find the IDs of the employees whose salary is strictly less than $30000 and whose manager left the company. 
+  When a manager leaves the company, their information is deleted from the Employees table, but the reports still have their manager_id set to the manager that left.
+  Return the result table ordered by employee_id.
+  The result format is in the following example.
   Table: Employees
   +-------------+----------+
   | Column Name | Type     |
