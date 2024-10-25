e1a6ecac8f498eb7de98daa7e8ea0530721608b6
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2024-10-25 19:50:38 +0900

    Update [3] Basic Aggregate Functions med - 550.sql

--- MySQL/Leetcode/SQL50/[3] Basic Aggregate Functions med - 550.sql
+++ MySQL/Leetcode/SQL50/[3] Basic Aggregate Functions med - 550.sql
@@ -35,18 +35,14 @@ SELECT  player_id
 -- " logged in again on the day after the day they first logged in " so MIN(event_date)
 -- 첫 로그인 다음 날에도 로그인을 한 사람의 비율을 구하는 것이 문제이다.
 -- group by를 처음부터 붙이면 LEAD(event_date, 1)의 의미가 없어진다.
--- 따라서 WHERE 를 붙이고 검색하면 우선 2번이상 접속한 사람 중 이어서 접속한 사람은 뜬다.
+-- 따라서 WHERE를 붙이고 검색하면 우선 2번이상 접속한 사람 중 이어서 접속한 사람은 뜬다.
+-- 하지만, 어차피 "PARTITION BY player_id ORDER BY event_date"를 하기 때문에 유저별 2회 이상 접속을 검색할 필요는 없다.
+-- 따라서 WHERE로 검색했던 "HAVING COUNT(event_date) > 1"은 필요가 없어진다.
 -- 그럼 이제 첫 event_date인지 아닌지 검사하는 것을 한 뒤
 -- 모든 player_id 중 몇 명인지 집계하면 될 것 같다.
 SELECT  player_id
       , event_date
       , CASE WHEN event_date = LEAD(event_date, 1) OVER (PARTITION BY player_id ORDER BY event_date) -1 THEN 1 ELSE 0 END AS next_date
   FROM  ACTIVITY
---  WHERE  player_id IN (
---                         SELECT  player_id
---                         FROM  ACTIVITY
---                         GROUP  BY player_id
---                         HAVING  COUNT(event_date) > 1
---                      )
  GROUP  BY player_id
 HAVING  MIN(event_date) = event_date;
