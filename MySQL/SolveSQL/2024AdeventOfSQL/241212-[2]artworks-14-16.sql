688d27250febdd9c279f8f648b809a5bc0638440
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-01-21 22:55:57 +0900

    Update 241212-[2]artworks-14-16.sql

--- MySQL/SolveSQL/2024AdeventOfSQL/241212-[2]artworks-14-16.sql
+++ MySQL/SolveSQL/2024AdeventOfSQL/241212-[2]artworks-14-16.sql
@@ -29,3 +29,13 @@ SELECT  classification
  GROUP  BY 1
  ORDER  BY 1
 ;
+
+
+-- 250121: 2014년만 해본 것인데 결과행이 나오지 않는다.
+SELECT  DISTINCT main_aw.classification AS classification
+      , (SELECT COUNT(*)  FROM artworks a14 WHERE main_aw.classification = a14.classification 
+                                              AND YEAR(a14.acquisition_date) = 2014)  AS '2014'
+  FROM  (SELECT IFNULL(classification, '(not assigned)') AS classification FROM artworks) main_aw
+
+ORDER  BY 1;
+-- 일단 전에 했던 것에서 두번째쿼리를 보면 메인쿼리 프롬절의 tmp 테이블이 WHERE로 기간이 제한되어서 classification의 종류가 제한되어 있을 수밖에 없다는 문제점을 발견했다.
