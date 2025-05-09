412378855348f2fd3287efd6bf0595db9c83953b
Jung Eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-05-09 22:16:08 +0900

    Create 015_.sql

---
+++ MySQL/SolveSQL/015_.sql
@@ -0,0 +1,15 @@
+/*
+https://solvesql.com/problems/settled-sellers-2/
+*/
+
+
+
+-- 250509:
+SELECT  seller_id
+      , COUNT(DISTINCT order_id) AS orders  
+  FROM  olist_order_items_dataset
+ WHERE  price >= 50 
+ GROUP  BY 1
+HAVING  COUNT(DISTINCT order_id) >= 100
+ ORDER  BY 2 DESC
+ ;
