1438086f1c6e6417e9f0da80dc4464372221d8e2
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-06-30 23:48:47 +0900

    Create 250630-Lv0_series-PCCE8.py

---
+++ python3/algorithm_programmers/250630-Lv0_series-PCCE8.py
@@ -0,0 +1,21 @@
+# PCCE 8번 디버깅: 한 줄만 수정하기
+# if 대신 while을, 3대신 4를 넣어야 맞다.
+
+def solution(nickname):
+    answer = ""
+    for letter in nickname:
+        if letter == "l":
+            answer += "I"
+        elif letter == "w":
+            answer += "vv"
+        elif letter == "W":
+            answer += "VV"
+        elif letter == "O":
+            answer += "0"
+        else:
+            answer += letter
+    while len(answer) < 4:    # 원본:if len(answer) < 3:
+        answer += "o"
+    if len(answer) > 8:
+        answer = answer[:8]
+    return answer
