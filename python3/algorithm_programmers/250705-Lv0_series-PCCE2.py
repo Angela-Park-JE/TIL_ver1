56eb0039e06d3b3ab53ab4f8eaf7cab08785288f
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-07-04 00:27:17 +0900

    Update 250705-Lv0_series-PCCE2.py

--- python3/algorithm_programmers/250705-Lv0_series-PCCE2.py
+++ python3/algorithm_programmers/250705-Lv0_series-PCCE2.py
@@ -1,4 +1,4 @@
-# PCCE2 디버깅 
+## PCCE2 디버깅 
 # https://school.programmers.co.kr/learn/courses/30/lessons/250132
 # 직각삼각형에서 빗변과 한 변이 주어졌을 때 다른 한 변의 제곱
 
@@ -10,7 +10,7 @@ print(b_square)
 
 
 
-# PCCE3 빈칸 채우기 
+## PCCE3 빈칸 채우기 
 # https://school.programmers.co.kr/learn/courses/30/lessons/250131
 # 문제 읽고 그저 빈칸을... 왜 이런문제가 나오지 : 연나이와 만나이
 year = int(input())
