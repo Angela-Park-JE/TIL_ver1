af953a4804cd528c788b451f260b9860718c9294
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-07-05 23:51:33 +0900

    Update 250706-Lv0_series-PCCE4.py

--- python3/algorithm_programmers/250706-Lv0_series-PCCE4.py
+++ python3/algorithm_programmers/250706-Lv0_series-PCCE4.py
@@ -18,4 +18,22 @@ print(month)
 
 
 
-## PCCE5 : 
+## ---
+# 250707
+## PCCE5 : 빈칸채우기
+# https://school.programmers.co.kr/learn/courses/30/lessons/250129
+
+def solution(route):
+    east = 0
+    north = 0
+    for i in route:
+        if i == "N":
+            north += 1
+        elif i == "S" :
+            north -= 1
+        elif i == "E" :
+            east += 1
+        elif i == 'W' :
+            east -= 1
+
+    return [east, north]
