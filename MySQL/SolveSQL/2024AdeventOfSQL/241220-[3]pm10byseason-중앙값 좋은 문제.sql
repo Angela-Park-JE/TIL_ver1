2f3a5d7af006bd65b0303e265a743af0e1eab121
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-07-24 00:56:29 +0900

    Update and rename 241220-[3]pm10byseason.sql to 241220-[3]pm10byseason-중앙값 좋은 문제.sql

--- MySQL/SolveSQL/2024AdeventOfSQL/241220-[3]pm10byseason-중앙값 좋은 문제.sql
+++ MySQL/SolveSQL/2024AdeventOfSQL/241220-[3]pm10byseason-중앙값 좋은 문제.sql
@@ -4,7 +4,52 @@ https://solvesql.com/problems/finedust-seasonal-summary/
 */
 
 
+-- 250723: 아래 먼저 푼 1번 방법보다 더 나은 방법(+ cte에 대해 table이라는 이름을 빼고 _cte 라고 바꾸었다), 딱히 짧아진건 아니나 ROW_NUMBER를 한 번만 해서 성능이 더 낫다고 함.
+-- 물론 원래 방법-줄세웠을 때 rownumber가 같은 혹은 비슷한 로우 끼리 제한해서 묶는 방법-의 문제점을 해결하기 위해 rankstable에 ORDER BY pm10 DESC, measured_at DESC 라는 조건을 달아줬다. measured_at이 겹치는 일은 없을테니까 말이다.
+-- 문제는 다른 상황에서 다른 컬럼을 가지고 해야할 때에는 이 방법이 어려울 수도 있다. 따라서 다른 방법을 찾아보던 중 더 나은 방법을 찾아 만든 쿼리이다.
+-- 이 방법은 전체 row 개수에 +1 을 한 수를 2로 나누어, 짝수도 홀수도 문제 없도록 FLOOR와 CEIL의 범위를 준 형식이다.
+-- 예를 들어 전체가 10개면 (10 + 1)/2 = 5.5, FLOOR와 CEIL을 적용하면 5, 6 둘을 선택하도록,
+-- 전체가 11개면 (11 + 1)/2 = 6, 올림과 내림을 적용하여도 6이니 6 하나를 선택하도록 고안되어있다.
+-- 2번 방법
+WITH 
+  season_cte AS   -- 테이블 전부 포함 및 season 정보
+  (
+    SELECT  *
+          , CASE WHEN MONTH(measured_at) BETWEEN  3 AND 5 THEN 'spring'
+                 WHEN MONTH(measured_at) BETWEEN  6 AND 8 THEN 'summer'
+                 WHEN MONTH(measured_at) BETWEEN  9 AND 11 THEN 'autumn'
+                 ELSE 'winter' 
+                 END AS season
+      FROM  measurements
+  )
+  , ranks_cte AS    -- row number는 한 번만 매기고 해당 그룹의 총 개수를 'cnt_byseason_div2'라는 이름으로 세어둔다.
+  (
+    SELECT  *
+          , ROW_NUMBER() OVER (PARTITION BY season ORDER BY pm10 DESC, measured_at DESC) AS desc_ranks
+          , (CONVERT(COUNT(1) OVER (PARTITION BY season), DECIMAL) + 1)/2 AS cnt_byseason_div2  -- CAST가 계속 오류나서 DECIMAL로 해둠
+      FROM  season_cte
+  )
+  , seasons_avg_cte AS   -- 시즌별 평균 정보
+  (
+    SELECT  season
+          , ROUND(AVG(pm10), 2) AS pm10_average
+      FROM  ranks_cte
+     GROUP  BY 1
+  )
+
+
+SELECT  season
+      , AVG(pm10) AS pm10_median
+      , (SELECT pm10_average FROM seasons_avg_cte s WHERE s.season = r.season) AS pm10_average
+  FROM  ranks_cte r
+ WHERE  desc_ranks BETWEEN FLOOR(cnt_byseason_div2) AND CEIL(cnt_byseason_div2)
+ GROUP  BY 1
+
+;
+
+
 -- 250723: WITH문 많아도 딱 세 개만 쓰고 풀자 했는데 그렇게 됐다. 
+-- 1번 방법
 WITH 
   seasontable AS   -- 테이블 전부 포함 및 season 정보
   (
