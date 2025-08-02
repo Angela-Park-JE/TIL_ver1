650f6c85fd72ee91e7beb4d8992e88d5ff886a82
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2025-08-02 17:31:59 +0900

    Update 241222-[3]counting-friends.sql

--- MySQL/SolveSQL/2024AdeventOfSQL/241222-[3]counting-friends.sql
+++ MySQL/SolveSQL/2024AdeventOfSQL/241222-[3]counting-friends.sql
@@ -1,6 +1,7 @@
 /*
 Advent of SQL 2024> 친구 수 집계하기 
 https://solvesql.com/problems/number-of-friends/
+쓰는 방식에 대해서 잠깐 고민했는데 UNION은 그냥 그대로, 딱히 들여쓰거나 띄어쓰지 않고 모습 그대로 쓰는게 가르는 줄의 의미로써 읽혀질 것 같다. 
 */
 
 
@@ -8,22 +9,22 @@ https://solvesql.com/problems/number-of-friends/
 -- 뭔가 더 간단하게 된 느낌이다. 
 WITH 
   all_edges AS  -- 모든 친구 관계 목록
-  (
-    SELECT  user_a_id, user_b_id
-      FROM  edges
-    UNION ALL
-    SELECT  user_b_id, user_a_id
-      FROM  edges
-  )
+    (
+      SELECT  user_a_id, user_b_id
+        FROM  edges
+      UNION ALL
+      SELECT  user_b_id, user_a_id
+        FROM  edges
+    )
   ,
   all_users AS  -- 모든 유저 목록
-  (
-    SELECT  user_id
-      FROM  users 
-    UNION
-    SELECT  DISTINCT user_a_id
-      FROM  all_edges
-  )
+    (
+      SELECT  user_id
+        FROM  users 
+      UNION
+      SELECT  DISTINCT user_a_id
+        FROM  all_edges
+    )
 
 SELECT  user_id
       , COUNT(user_b_id) AS num_friends
