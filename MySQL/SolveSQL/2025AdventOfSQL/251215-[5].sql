6371af11509166c01498c2e08e216df43f383fb2
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2026-03-30 23:53:35 +0900

    Update 251215-[5].sql

--- MySQL/SolveSQL/2025AdventOfSQL/251215-[5].sql
+++ MySQL/SolveSQL/2025AdventOfSQL/251215-[5].sql
@@ -1,3 +1,23 @@
 /*
-진짜 풀어내고 싶다...
+한국 감독 영화
+https://solvesql.com/problems/find-movies-by-korean-artists/
 */
+
+
+
+-- 251215: 260330 쉽게 풀어버린
+-- -- 먼저 한국은 어떤식으로 표기되어있나 확인 후 
+-- SELECT  DISTINCT nationality
+-- FROM  artists
+-- ORDER  BY 1
+-- ;
+-- -- 'Korean' 이라고 되어있음
+
+SELECT  arti.name AS artist
+      , aw.title AS title
+  FROM  artworks aw
+        LEFT JOIN artworks_artists aa ON aw.artwork_id = aa.artwork_id
+        LEFT JOIN artists arti ON aa.artist_id = arti.artist_id 
+ WHERE  aw.classification LIKE 'Film%' 
+   AND  arti.nationality = 'Korean'
+;
