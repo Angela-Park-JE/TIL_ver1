e549a20b67225601558cafc9f402d658fb26bd7a
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2026-04-08 04:03:24 +0900

    Update 251216-[3].sql

--- MySQL/SolveSQL/2025AdventOfSQL/251216-[3].sql
+++ MySQL/SolveSQL/2025AdventOfSQL/251216-[3].sql
@@ -2,3 +2,10 @@
 초기 형성된 친구 관계 분석
 https://solvesql.com/problems/friendship-between-early-users/
 */
+
+
+-- 우선 users를 보니 feature_ids 컬럼에 친구 아이디가 콤마로 구분되어있음. 몇 명 있는지 다 다르고 계산이 까다로워지는 부분이다.
+SELECT  *
+  FROM  users
+
+  LIMIT 100 ;
