0ccc7821e89505e471d0a829e0e6e66b4826c5a2
Jung-eun Park <88322298+Angela-Park-JE@users.noreply.github.com>
2026-04-28 23:19:49 +0900

    Update 026.sql

--- MySQL/SolveSQL/026.sql
+++ MySQL/SolveSQL/026.sql
@@ -2,3 +2,10 @@
 Rolling retention
 https://solvesql.com/problems/monthly-rolling-retention/
 */
+
+
+/*풀이과정*/
+-- 우선 계산해야한 리텐션 기간이 언제까지인지 보려고 한다
+SELECT  MIN(last_order_date), MAX(last_order_date)
+  FROM  customer_stats
+;
